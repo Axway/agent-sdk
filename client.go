49a1a665cf913b2c452936aa1f5dcb97711aaf46
dfeldick <dfeldick@axway.com>
2019-12-17 13:10:59 -0700

    APIGOV-12804 - more TLSConfig changes

--- client.go
+++ client.go
@@ -1,12 +1,10 @@
 package apic
 
 import (
-	"bytes"
 	"encoding/json"
 	"errors"
-	"io"
-	"io/ioutil"
 	"net/http"
+	"strconv"
 
 	corecfg "git.ecd.axway.int/apigov/aws_apigw_discovery_agent/core/config"
 	coreapi "git.ecd.axway.int/apigov/aws_apigw_discovery_agent/pkg/api"
@@ -58,7 +56,7 @@ type Service struct {
 type Client struct {
 	tokenRequester *apicauth.PlatformTokenGetter
 	cfg            corecfg.CentralConfig
-	apiClient      *coreapi.Client //
+	apiClient      *coreapi.Client
 }
 
 // New -
@@ -103,33 +101,45 @@ func SetLog(newLog logrus.FieldLogger) {
 
 // DeployAPI -
 func (c *Client) DeployAPI(service Service) (string, error) {
-	request, err := setHeader(c, service.Method, service.URL, bytes.NewBuffer(service.Buffer))
-
+	headers, err := c.createHeader()
 	if err != nil {
 		return "", err
 	}
 
-	response, err := c.apiClient.GetHTTPClient().Do(request)
+	request := coreapi.Request{
+		Method:      service.Method,
+		URL:         service.URL,
+		QueryParams: nil,
+		Headers:     headers,
+		Body:        service.Buffer,
+	}
+	response, err := c.apiClient.Send(request)
 	if err != nil {
 		return "", err
 	}
-
-	if !(response.StatusCode == http.StatusOK || response.StatusCode == http.StatusCreated) {
-		detail := make(map[string]*json.RawMessage)
-		json.NewDecoder(response.Body).Decode(&detail)
-		for k, v := range detail {
-			buffer, _ := v.MarshalJSON()
-			log.Debugf("HTTP response key %v: %v", k, string(buffer))
-		}
-		return "", errors.New(response.Status)
-	}
-	defer response.Body.Close()
-	body, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		return "", err
+	if !(response.Code == http.StatusOK || response.Code == http.StatusCreated) {
+		logResponseErrors(response.Body)
+		return "", errors.New(strconv.Itoa(response.Code))
 	}
 
-	return handleResponse(service.AgentMode, body)
+	return handleResponse(service.AgentMode, response.Body)
+}
+
+type apiErrorResponse map[string][]apiError
+
+type apiError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func logResponseErrors(body []byte) {
+	detail := make(map[string]*json.RawMessage)
+	json.Unmarshal(body, &detail)
+
+	for k, v := range detail {
+		buffer, _ := v.MarshalJSON()
+		log.Debugf("HTTP response %v: %v", k, string(buffer))
+	}
 }
 
 func handleResponse(agentMode corecfg.AgentMode, body []byte) (string, error) {
@@ -151,17 +161,14 @@ func handleResponse(agentMode corecfg.AgentMode, body []byte) (string, error) {
 	return itemID, nil
 }
 
-// SetHeader - set header
-func setHeader(c *Client, method, url string, body io.Reader) (*http.Request, error) {
-	request, err := http.NewRequest(method, url, body)
-	var token string
-
-	if token, err = c.tokenRequester.GetToken(); err != nil {
+func (c *Client) createHeader() (map[string]string, error) {
+	token, err := c.tokenRequester.GetToken()
+	if err != nil {
 		return nil, err
 	}
-
-	request.Header.Add("X-Axway-Tenant-Id", c.cfg.GetTenantID())
-	request.Header.Add("Authorization", "Bearer "+token)
-	request.Header.Add("Content-Type", "application/json")
-	return request, nil
+	headers := make(map[string]string)
+	headers["X-Axway-Tenant-Id"] = c.cfg.GetTenantID()
+	headers["Authorization"] = "Bearer " + token
+	headers["Content-Type"] = "application/json"
+	return headers, nil
 }
