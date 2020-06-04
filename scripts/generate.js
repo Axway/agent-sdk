15ea12017a1f79f82a6a3c95b8b3cc5282b9ed43
Trevor Johnson <tjohnson@axway.com>
2020-06-04 09:00:34 -0700

    APIGOV-14999_tmp update

---
+++ scripts/generate.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const { exec, execSync } = require('child_process');
+
+const apiserverDir = path.join(__dirname, '..', 'pkg/apic/apiserver');
+const project = 'git.ecd.axway.int/apigov/apic_agents_sdk';
+
+const files = fs.readdirSync(apiserverDir);
+
+for (file of files) {
+  const dirToMake = file.split('_');
+  const [, group, kind] = dirToMake;
+  if (dirToMake.length > 2) {
+    const modelsDir = `${apiserverDir}/models/${group}/${kind}`;
+    execSync(`mkdir -p ${modelsDir}`);
+    const currentFile = `${apiserverDir}/${file}`;
+    const fileContent = fs.readFileSync(currentFile, { encoding: 'utf-8' });
+    const newContent = fileContent
+      .replace(
+        'package v1',
+        `package ${kind}\n import v1 "${project}/pkg/apic/apiserver/models/api/v1"`
+      )
+      .replace(/ ApiV1/, ' v1.ApiV1');
+    fs.writeFile(currentFile, newContent, { encoding: 'utf-8' }, (err) => {
+      if (err) console.error('ERROR: ', err);
+      exec(`mv ${currentFile} ${modelsDir}`);
+    });
+  }
+}
--- scripts/group.jq
+++ scripts/group.jq
@@ -10,8 +10,8 @@ group_by({group,version}) | map(
                                   "openapi": {
                                      "components": {
                                          "schemas": ({}
-                                                       + (map(. as $root | .subresources |  if . then with_entries({"key": "\($root.kind)\(.key | ascii_upcase)", "value": .value.openAPIV3Schema}) else {} end) | reduce .[] as $x ({}; . + $x))
-                                                       +  (map({"key": "\(.kind)SPEC", "value": .schema.openAPIV3Schema }) | from_entries))
+																							+ (map(. as $root | .subresources |  if . then with_entries({"key": "\($root.kind)\(.key | ascii_upcase)", "value": .value.openAPIV3Schema}) else {} end) | reduce .[] as $x ({}; . + $x))
+																							+  (map({"key": "\(.kind)SPEC", "value": .schema.openAPIV3Schema }) | from_entries))
                                       },
                                    "paths": {},
                                    "openapi" : "3.0.2",
