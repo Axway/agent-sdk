18483f2e3f5618637936468c4098742b87261de7
dfeldick <dfeldick@axway.com>
2020-06-08 14:23:46 -0700

    APIGOV-11516 - comments

--- pkg/apic/apiService.go
+++ pkg/apic/apiService.go
@@ -226,7 +226,7 @@ func (c *ServiceClient) processUnpublish(httpMethod, consumerInstanceName string
 		return err
 	}
 
-	// this should start the removal of the subscriptions
+	// this will start the removal of the subscriptions
 	subscriptions, err := c.InitiateUnsubscribeCatalogItem(catalogItemID)
 	if err != nil {
 		log.Errorf("Unable to update subscriptions for catalog with ID '%s'. %v", catalogItemID, err.Error())
