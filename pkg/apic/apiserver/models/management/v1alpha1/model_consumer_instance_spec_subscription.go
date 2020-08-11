b63be42e1670e1ee0c29b5124acd909eca7dba15
dfeldick <dfeldick@axway.com>
2020-08-11 16:21:52 -0700

    APIGOV-15169 - temp changes to generated code

--- pkg/apic/apiserver/models/management/v1alpha1/model_consumer_instance_spec_subscription.go
+++ pkg/apic/apiserver/models/management/v1alpha1/model_consumer_instance_spec_subscription.go
@@ -14,7 +14,9 @@ type ConsumerInstanceSpecSubscription struct {
 	// Defines if subscriptions are allowed on the Catalog Item.
 	Enabled bool `json:"enabled,omitempty"`
 	// Defines if subscriptions need to be manually approved.
-	AutoSubscribe bool `json:"autoSubscribe,omitempty"`
+	// TODO changed
+	// AutoSubscribe bool `json:"autoSubscribe,omitempty"`
+	AutoSubscribe bool `json:"autoSubscribe"`
 	// The name of a ConsumerSubscriptionDefinition kind that defines the schema and possible webhooks to get invoked.
 	SubscriptionDefinition string `json:"subscriptionDefinition,omitempty"`
 }
