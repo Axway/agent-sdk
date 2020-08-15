9c712056bf1693fcc56f1f4d1957610aff39af89
dfeldick <dfeldick@axway.com>
2020-08-15 11:24:14 -0700

    APIGOV-15851 - fix error messages

--- pkg/filter/errors.go
+++ pkg/filter/errors.go
@@ -8,7 +8,7 @@ var (
 	ErrFilterExpression    = errors.New(1148, "error parsing APIMANAGER_FILTER filter in configuration. Unrecognized expression")
 	ErrFilterGeneralParse  = errors.New(1149, "error parsing APIMANAGER_FILTER filter in configuration")
 	ErrFilterArgument      = errors.New(1150, "error parsing APIMANAGER_FILTER filter in configuration. Invalid call argument")
-	ErrFilterSelectorType  = errors.New(1147, "error parsing APIMANAGER_FILTER filter in configuration. Invalid selector type")
+	ErrFilterSelectorType  = errors.New(1151, "error parsing APIMANAGER_FILTER filter in configuration. Invalid selector type")
 	ErrFilterSelectorExpr  = errors.New(1152, "error parsing APIMANAGER_FILTER filter in configuration. Invalid selector expression")
 	ErrFilterOperator      = errors.New(1153, "error parsing APIMANAGER_FILTER filter in configuration. Invalid operator")
 	ErrFilterCondition     = errors.New(1154, "error parsing APIMANAGER_FILTER filter in configuration. Unrecognized condition")
