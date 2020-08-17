e8321947cf60da7e9da76660470b9e826d86e306
dfeldick <dfeldick@axway.com>
2020-08-17 08:17:21 -0700

    APIGOV-15851 - make filter errors more generic

--- pkg/filter/errors.go
+++ pkg/filter/errors.go
@@ -4,12 +4,12 @@ import "git.ecd.axway.org/apigov/apic_agents_sdk/pkg/util/errors"
 
 // Errors hit while parsing the APIMANAGER_FILTER
 var (
-	ErrFilterConfiguration = errors.Newf(1147, "error parsing APIMANAGER_FILTER filter in configuration. Syntax error:%s")
-	ErrFilterExpression    = errors.New(1148, "error parsing APIMANAGER_FILTER filter in configuration. Unrecognized expression")
-	ErrFilterGeneralParse  = errors.New(1149, "error parsing APIMANAGER_FILTER filter in configuration")
-	ErrFilterArgument      = errors.New(1150, "error parsing APIMANAGER_FILTER filter in configuration. Invalid call argument")
-	ErrFilterSelectorType  = errors.New(1151, "error parsing APIMANAGER_FILTER filter in configuration. Invalid selector type")
-	ErrFilterSelectorExpr  = errors.New(1152, "error parsing APIMANAGER_FILTER filter in configuration. Invalid selector expression")
-	ErrFilterOperator      = errors.New(1153, "error parsing APIMANAGER_FILTER filter in configuration. Invalid operator")
-	ErrFilterCondition     = errors.New(1154, "error parsing APIMANAGER_FILTER filter in configuration. Unrecognized condition")
+	ErrFilterConfiguration = errors.Newf(1147, "error parsing filter in configuration. Syntax error:%s")
+	ErrFilterExpression    = errors.New(1148, "error parsing filter in configuration. Unrecognized expression")
+	ErrFilterGeneralParse  = errors.New(1149, "error parsing filter in configuration")
+	ErrFilterArgument      = errors.New(1150, "error parsing filter in configuration. Invalid call argument")
+	ErrFilterSelectorType  = errors.New(1151, "error parsing filter in configuration. Invalid selector type")
+	ErrFilterSelectorExpr  = errors.New(1152, "error parsing filter in configuration. Invalid selector expression")
+	ErrFilterOperator      = errors.New(1153, "error parsing filter in configuration. Invalid operator")
+	ErrFilterCondition     = errors.New(1154, "error parsing filter in configuration. Unrecognized condition")
 )
