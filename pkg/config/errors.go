fbe4403fe3a4979c55edcda6fac0892b4b15d0ed
sbolosan@axway.com <sbolosan@axway.com>
2020-08-20 15:36:42 -0700

    APIGOV-15959

--- pkg/config/errors.go
+++ pkg/config/errors.go
@@ -2,7 +2,7 @@ package config
 
 import configerrors "git.ecd.axway.org/apigov/apic_agents_sdk/pkg/util/errors"
 
-// Errors hit when sending subscription notifications
+// Errors hit when validating or parsing config
 var (
-	ErrSubscriptionApprovalModeInvalid = configerrors.New(1301, "error central.subscriptions.approvalmode set to incorrect value in config")
+	ErrSubscriptionApprovalModeInvalid = configerrors.New(1401, "error central.subscriptions.approvalmode set to an incorrect value in config")
 )
