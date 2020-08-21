aacd94320cc6c37eb838fec3702c8191fe60f1a5
Jason Collins <jcollins@axway.com>
2020-08-21 15:15:28 -0700

    APIGOV-15987 - throw error if removing a running service, move errors to errors module and use errors package

--- pkg/cmd/service/daemon/daemonlinux.go
+++ pkg/cmd/service/daemon/daemonlinux.go
@@ -1,16 +1,12 @@
 package daemon
 
-import (
-	"fmt"
-)
-
 // Get the daemon properly
 func newDaemon(name, description string, dependencies []string) (Daemon, error) {
 	// newer subsystem must be checked first
 	if _, err := fs.Stat("/run/systemd/system"); err == nil {
 		return &systemDRecord{name, description, dependencies, "", ""}, nil
 	}
-	return nil, fmt.Errorf("cannot install service, need systemd")
+	return nil, ErrUnsupportedSystem
 }
 
 // Get executable path
