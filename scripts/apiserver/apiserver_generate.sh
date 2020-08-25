97a3f993fa4ed5f35c5331fe4646086d17123b57
dfeldick <dfeldick@axway.com>
2020-08-25 11:33:19 -0700

    APIGOV-16014 - comment and update scripts

--- scripts/apiserver/apiserver_generate.sh
+++ scripts/apiserver/apiserver_generate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# set thes environment vars
+export GO_POST_PROCESS_FILE="/usr/local/go/bin/gofmt -w"
+export GO111MODULE=on
+
 node ./scripts/apiserver/generate.js
-go fmt ./pkg/apic/apiserver/...
+
+## just in case, update all go imports
 goimports -w=true ./pkg/apic/apiserver/models
--- scripts/unifiedcatalog/unifiedcatalog_generate.sh
+++ scripts/unifiedcatalog/unifiedcatalog_generate.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+# set thes environment vars
+export GO_POST_PROCESS_FILE="/usr/local/go/bin/gofmt -w"
+export GO111MODULE=on
+
 node ./scripts/unifiedcatalog/generate.js
-go fmt ./pkg/apic/unifiedcatalog/models/...
+
+## just in case, update all go imports
 goimports -w=true ./pkg/apic/unifiedcatalog/models
