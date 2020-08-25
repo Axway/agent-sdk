05612adf5bfc0f29f0a4076aabc0c5a579dfd35f
dfeldick <dfeldick@axway.com>
2020-08-25 12:57:39 -0700

    APIGOV-16014 - remove path to gofmt

--- scripts/apiserver/apiserver_generate.sh
+++ scripts/apiserver/apiserver_generate.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # set thes environment vars
-export GO_POST_PROCESS_FILE="/usr/local/go/bin/gofmt -w"
+export GO_POST_PROCESS_FILE="`command -v gofmt` -w"
 export GO111MODULE=on
 
 node ./scripts/apiserver/generate.js
