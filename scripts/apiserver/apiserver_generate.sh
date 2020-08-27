7659bf11077ba4e77af2468789858b8704d1b948
dfeldick <dfeldick@axway.com>
2020-08-27 11:37:56 -0700

    APIGOV-16018 - add script to fix model_consumer_instance_spec_subscription.go

--- scripts/apiserver/apiserver_generate.sh
+++ scripts/apiserver/apiserver_generate.sh
@@ -6,5 +6,8 @@ export GO111MODULE=on
 
 node ./scripts/apiserver/generate.js
 
-## just in case, update all go imports
+# just in case, update all go imports
 goimports -w=true ./pkg/apic/apiserver/models
+
+# run script to modify any files that need tweeking
+./scripts/apiserver/modify_models.sh
