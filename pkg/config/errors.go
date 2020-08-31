80789fe4c7fd7e51afa3c30cc9df0ae3c35d35a0
Vivek Chauhan <vchauhan@axway.com>
2020-08-31 15:14:06 +0530

    APIGOV-16008 - Adding support for envFile flag in agents to override config using environment variables

--- pkg/config/errors.go
+++ pkg/config/errors.go
@@ -5,4 +5,5 @@ import configerrors "git.ecd.axway.org/apigov/apic_agents_sdk/pkg/util/errors"
 // Errors hit when validating or parsing config
 var (
 	ErrSubscriptionApprovalModeInvalid = configerrors.New(1401, "error central.subscriptions.approvalmode set to an incorrect value in config")
+	ErrEnvConfigOverride               = configerrors.Newf(1402, "error in overriding configuration using environment variables")
 )
