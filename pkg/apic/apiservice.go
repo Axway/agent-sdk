c55f3d18890854f39fa8fa5d1d152cbf69c27668
sbolosan@axway.com <sbolosan@axway.com>
2020-10-07 07:25:48 -0700

    INT - remove paren after stage

--- pkg/apic/apiservice.go
+++ pkg/apic/apiservice.go
@@ -90,7 +90,7 @@ func (c *ServiceClient) updateService(serviceBody ServiceBody) (string, error) {
 // Update description and title after updating or creating APIService to inlcude the stage name if it exists
 func (c *ServiceClient) postAPIServiceUpdate(serviceBody ServiceBody) ServiceBody {
 	if serviceBody.Stage != "" {
-		serviceBody.Description = serviceBody.Description + ", StageName: " + serviceBody.Stage + ")"
+		serviceBody.Description = serviceBody.Description + ", StageName: " + serviceBody.Stage
 		serviceBody.NameToPush = fmt.Sprintf("%v (Stage: %v)", serviceBody.NameToPush, serviceBody.Stage)
 	}
 
