2ad9bd9a456f7005aa3ef5a1842b9f6f3aea48d3
dfeldick <dfeldick@axway.com>
2020-10-13 11:40:24 -0700

    APIGOV-16177 - remove error

--- pkg/apic/errors.go
+++ pkg/apic/errors.go
@@ -29,5 +29,4 @@ var (
 	ErrSubscriptionSchemaGet           = errors.New(1155, "error getting subscription schema in AMPLIFY Central")
 	ErrGetSubscriptionDefProperties    = errors.New(1156, "error getting subscription definition properties in AMPLIFY Central")
 	ErrUpdateSubscriptionDefProperties = errors.New(1157, "error updating subscription definition properties in AMPLIFY Central")
-	ErrGetSubscriptionSchema           = errors.New(1158, "error getting subscription schema in AMPLIFY Central")
 )
