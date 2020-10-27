af65e8236a65299102474045bbe577fb122d6383
dfeldick <dfeldick@axway.com>
2020-10-27 15:01:22 -0700

    APIGOV-16552 - fix webhook validations

--- pkg/config/webhookconfig.go
+++ pkg/config/webhookconfig.go
@@ -17,7 +17,7 @@ type WebhookConfig interface {
 	ValidateConfig() error
 }
 
-// WebhookConfiguration -
+// WebhookConfiguration - do NOT make this an IConfigValidator, as it is validated as part of subscriptionConfig
 type WebhookConfiguration struct {
 	WebhookConfig
 	URL            string `config:"url"`
@@ -51,26 +51,31 @@ func (c *WebhookConfiguration) GetSecret() string {
 	return c.Secret
 }
 
-// ValidateConfig - Validate the config
+// ValidateConfig - Validate the config. Do NOT make this ValidateCfg IConfigValidator. It is called directly from the subscriptionconfig
+// validator. But, it is ONLY called if the approvalMode is "webhook"
 func (c *WebhookConfiguration) ValidateConfig() error {
-	if webhookURL := c.GetURL(); webhookURL != "" {
+	if c.IsConfigured() {
+		webhookURL := c.GetURL()
 		if _, err := url.ParseRequestURI(webhookURL); err != nil {
 			return errors.New("central.subscriptions.approvalWebhook.URL is not a valid URL")
 		}
 
-		// (example header) Header=contentType,Value=application/json, Header=Elements-Formula-Instance-Id,Value=440874, Header=Authorization,Value=User F+rYQSfu0w5yIa5q7uNs2MKYcIok8pYpgAUwJtXFnzc=, Organization a1713018bbde8f54f4f55ff8c3bd8bfe
-		c.webhookHeaders = map[string]string{}
-		c.Headers = strings.Replace(c.Headers, ", ", ",", -1)
-		headersValues := strings.Split(c.Headers, ",Header=")
-		for _, headerValue := range headersValues {
-			hvArray := strings.Split(headerValue, ",Value=")
-			if len(hvArray) != 2 {
-				return errors.New("could not parse value of central.subscriptions.approvalWebhook.headers")
+		// headers are allowed to be empty, so only validate if there is a configured value
+		if c.Headers != "" {
+			// (example header) Header=contentType,Value=application/json, Header=Elements-Formula-Instance-Id,Value=440874, Header=Authorization,Value=User F+rYQSfu0w5yIa5q7uNs2MKYcIok8pYpgAUwJtXFnzc=, Organization a1713018bbde8f54f4f55ff8c3bd8bfe
+			c.webhookHeaders = map[string]string{}
+			c.Headers = strings.Replace(c.Headers, ", ", ",", -1)
+			headersValues := strings.Split(c.Headers, ",Header=")
+			for _, headerValue := range headersValues {
+				hvArray := strings.Split(headerValue, ",Value=")
+				if len(hvArray) != 2 {
+					return errors.New("could not parse value of central.subscriptions.approvalWebhook.headers")
+				}
+				hvArray[0] = strings.TrimLeft(hvArray[0], "Header=") // handle the first header in the list
+				c.webhookHeaders[hvArray[0]] = hvArray[1]
 			}
-			hvArray[0] = strings.TrimLeft(hvArray[0], "Header=") // handle the first	header in the list
-			c.webhookHeaders[hvArray[0]] = hvArray[1]
 		}
-		log.Debug("Subscription approval webhook notification set")
+		log.Debug("Subscription approval webhook configuration set")
 	}
 
 	return nil
