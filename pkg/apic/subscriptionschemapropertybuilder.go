5c0a77ccb25329e88e650194dad91c926a98f080
Dale Feldick <40580732+dfeldick@users.noreply.github.com>
2021-04-20 10:51:32 -0700

    Apigov 18461 (#81)
    * APIGOV-18461 - fix enum sorting
    
    * APIGOV-18461 - cleanup

--- pkg/apic/subscriptionschemapropertybuilder.go
+++ pkg/apic/subscriptionschemapropertybuilder.go
@@ -23,7 +23,7 @@ type schemaProperty struct {
 	name         string
 	description  string
 	apicRefField string
-	enums        map[string]bool
+	enums        []string
 	required     bool
 	readOnly     bool
 	hidden       bool
@@ -33,7 +33,7 @@ type schemaProperty struct {
 // NewSubscriptionSchemaPropertyBuilder - Creates a new subscription schema property builder
 func NewSubscriptionSchemaPropertyBuilder() SubscriptionPropertyBuilder {
 	return &schemaProperty{
-		enums: make(map[string]bool),
+		enums: make([]string, 0),
 	}
 }
 
@@ -51,16 +51,33 @@ func (p *schemaProperty) SetDescription(description string) SubscriptionProperty
 
 // SetEnumValues - add a list of enum values to the property
 func (p *schemaProperty) SetEnumValues(values []string) SubscriptionPropertyBuilder {
+	dict := make(map[string]bool, 0)
+
+	// use a temp map to filter out any duplicate values from the input
 	for _, value := range values {
-		p.enums[value] = true
+		if _, ok := dict[value]; !ok {
+			dict[value] = true
+			p.enums = append(p.enums, value)
+		}
 	}
+
 	return p
+}
+
+func (p *schemaProperty) enumContains(str string) bool {
+	for _, v := range p.enums {
+		if v == str {
+			return true
+		}
+	}
+
+	return false
 }
 
 // AddEnumValue - add a new value to the enum list
 func (p *schemaProperty) AddEnumValue(value string) SubscriptionPropertyBuilder {
-	if _, ok := p.enums[value]; !ok {
-		p.enums[value] = true
+	if !p.enumContains(value) {
+		p.enums = append(p.enums, value)
 	}
 	return p
 }
@@ -119,18 +136,11 @@ func (p *schemaProperty) Build() (*SubscriptionSchemaPropertyDefinition, error) 
 		APICRef:     p.apicRefField,
 		ReadOnly:    p.readOnly,
 		Required:    p.required,
+		Enum:        p.enums,
 	}
 
 	if p.hidden {
 		prop.Format = "hidden"
-	}
-	// Convert map to string array
-	if len(p.enums) > 0 {
-		list := make([]string, 0)
-		for key := range p.enums {
-			list = append(list, key)
-		}
-		prop.Enum = list
 	}
 
 	return prop, nil
