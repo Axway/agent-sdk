12ec151f2285bcd73a5fa595066774a467a87e24
Jason Collins <jcollins@axway.com>
2021-05-13 09:21:42 -0700

    APIGOV-17556 - fix deprecation warning message

--- pkg/transaction/eventgenerator.go
+++ pkg/transaction/eventgenerator.go
@@ -46,7 +46,7 @@ func NewEventGenerator() EventGenerator {
 
 // CreateEvent - Creates a new event to be sent to Amplify Observability
 func (e *Generator) CreateEvent(logEvent LogEvent, eventTime time.Time, metaData common.MapStr, eventFields common.MapStr, privateData interface{}) (beat.Event, error) {
-	log.Warnf("%s is deprecated, to enable sampling please start using %s", "CreateEvent", "CreateTransactionEvents")
+	log.Warnf("%s is deprecated, to enable sampling please start using %s", "CreateEvent", "CreateEvents")
 
 	// if CreateEvent is being used, sampling will not work, so all events need to be sent
 	if metaData == nil {
