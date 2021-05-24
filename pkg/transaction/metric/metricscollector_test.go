31c12845542ae8f4b026dcdb08bfd37bc96e1d38
Jason Collins <jcollins@axway.com>
2021-05-24 08:49:50 -0700

    APIGOV-18886 - fix test

--- pkg/transaction/metric/metricscollector_test.go
+++ pkg/transaction/metric/metricscollector_test.go
@@ -72,7 +72,7 @@ func TestMetricCollector(t *testing.T) {
 	defer s.Close()
 	cfg := createCentralCfg(s.URL, "demo")
 	cfg.LighthouseURL = s.URL + "/lighthouse"
-	cfg.PlatformEnvironmentID = "267bd671-e5e2-4679-bcc3-bbe7b70f30fd"
+	cfg.SetEnvironmentID("267bd671-e5e2-4679-bcc3-bbe7b70f30fd")
 	cmd.BuildDataPlaneType = "Azure"
 	agent.Initialize(cfg)
 
