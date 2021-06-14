8673668feb1033fca1e5ab39e792a6578a63894b
sbolosan@axway.com <sbolosan@axway.com>
2021-06-14 10:50:52 -0700

    APIGOV-19081 - update APIServer  using v1alpha.ResourceInstances

--- pkg/agent/discoverycache.go
+++ pkg/agent/discoverycache.go
@@ -3,7 +3,6 @@ package agent
 import (
 	"encoding/json"
 	"fmt"
-	"strconv"
 
 	coreapi "github.com/Axway/agent-sdk/pkg/api"
 	"github.com/Axway/agent-sdk/pkg/apic"
@@ -55,43 +54,24 @@ func (j *discoveryCache) Execute() error {
 
 func updateAPICache() {
 	log.Trace("updating API cache")
-	apiServerURL := agent.cfg.GetServicesURL()
 
 	// Update cache with published resources
 	existingAPIs := make(map[string]bool)
+	query := map[string]string{
+		"query":  attributesQueryParam + apic.AttrExternalAPIID + "!=\"\"",
+		"fields": apiServerFields,
+	}
 
-	morePages := true
-	page := 1
-	for morePages {
-		query := map[string]string{
-			"query":    attributesQueryParam + apic.AttrExternalAPIID + "!=\"\"",
-			"page":     strconv.Itoa(page),
-			"pageSize": strconv.Itoa(apiServerPageSize),
-			"fields":   apiServerFields,
+	apiServices, _ := GetCentralClient().GetAPIV1ResourceInstances(query, agent.cfg.GetServicesURL())
+
+	for _, apiService := range apiServices {
+		externalAPIID := addItemToAPICache(*apiService)
+		if externalAPIPrimaryKey, found := apiService.Attributes[apic.AttrExternalAPIPrimaryKey]; found {
+			existingAPIs[externalAPIPrimaryKey] = true
+		} else {
+			existingAPIs[externalAPIID] = true
 		}
 
-		response, err := agent.apicClient.ExecuteAPI(coreapi.GET, apiServerURL, query, nil)
-		if err != nil {
-			log.Debugf("Error while updating published API cache: %s", err.Error())
-			return
-		}
-
-		apiServices := make([]apiV1.ResourceInstance, 0)
-		json.Unmarshal(response, &apiServices)
-
-		for _, apiService := range apiServices {
-			externalAPIID := addItemToAPICache(apiService)
-			if externalAPIPrimaryKey, found := apiService.Attributes[apic.AttrExternalAPIPrimaryKey]; found {
-				existingAPIs[externalAPIPrimaryKey] = true
-			} else {
-				existingAPIs[externalAPIID] = true
-			}
-		}
-
-		if len(apiServices) < apiServerPageSize {
-			morePages = false
-		}
-		page++
 	}
 
 	// Remove items that are not published as Resources
