5b17650d25113f8b5fac396588db09dcbec805c1
sbolosan@axway.com <sbolosan@axway.com>
2021-07-06 11:37:45 -0700

    INT - change to Newf

--- pkg/util/errors/errors.go
+++ pkg/util/errors/errors.go
@@ -5,5 +5,5 @@ var (
 	ErrInitServicesNotReady      = New(1001, "failed to initialize. Services are not ready")
 	ErrTimeoutServicesNotReady   = New(1002, "failed with timeout error.  Services are not ready")
 	ErrPeriodicCheck             = Newf(1003, "%s failed.  Services are not ready")
-	ErrStartingAgentStatusUpdate = New(1004, "error starting %s update")
+	ErrStartingAgentStatusUpdate = Newf(1004, "error starting %s update")
 )
