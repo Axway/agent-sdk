00c0d3337e0e6d88452bd5198da51d5ff1570ff1
Sagar <skalburgi@axway.com>
2021-09-01 16:03:19 -0700

    APIGOV-19802 Test

--- pkg/agent/discoveryagents.go
+++ pkg/agent/discoveryagents.go
@@ -3,6 +3,8 @@ package agent
 import (
 	"strings"
 
+	"github.com/Axway/agent-sdk/pkg/util/log"
+
 	apiV1 "github.com/Axway/agent-sdk/pkg/apic/apiserver/models/api/v1"
 	"github.com/Axway/agent-sdk/pkg/apic/apiserver/models/management/v1alpha1"
 	"github.com/Axway/agent-sdk/pkg/config"
@@ -16,6 +18,8 @@ func discoveryAgent(res *apiV1.ResourceInstance) *v1alpha1.DiscoveryAgent {
 }
 
 func createDiscoveryAgentStatusResource(status, message string) *v1alpha1.DiscoveryAgent {
+	log.Info("Discovery agent's latest version is: ", config.AgentLatestVersion)
+
 	agentRes := v1alpha1.DiscoveryAgent{}
 	agentRes.Name = agent.cfg.GetAgentName()
 	agentRes.Status.Version = config.AgentVersion
