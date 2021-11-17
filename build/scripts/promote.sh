52015d9313010a5ee9e52787105b07e7a7c39e79
Dale Feldick <40580732+dfeldick@users.noreply.github.com>
2021-11-17 13:48:04 -0700

    APIGOV-20671 - automate the SDK release process (#249)
    * APIGOV-12345 - play with action for release
    
    * APIGOV-12345 - more
    
    * APIGOV-12345 - fix
    
    * APIGOV-12345 - fix
    
    * APIGOV-12345 - change
    
    * APIGOV-12345 - changes
    
    * APIGOV-12345 - remove branch for now
    
    * APIGOV-12345 - fix scrpt
    
    * APIGOV-12345 - fix commit
    
    * APIGOV-12345 - fix commit
    
    * APIGOV-12345 - fix
    
    * INT - update to new release 1.1.8
    
    * APIGOV-12345 - final?
    
    * APIGOV-12345 - final
    
    * APIGOV-12345 - prepare for main
    
    * APIGOV-12345 - cleanup
    
    * empty commit to push new branch
    
    * APIGOV-20671 - fix regex for semantic versioning
    
    * APIGOV-20671 - fix version
    
    Co-authored-by: Gitlab action <gitaction@axway.com>

---
+++ build/scripts/promote.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+check_required_param() {
+    echo $1
+    if [ -z $1 ]; then
+        return 1
+    fi
+
+    pat='v[0-9]+\.[0-9]+\.[0-9]+'
+    if [[ $1 =~ $pat ]]; then
+        return 0
+    fi
+    return 1
+}
+
+set_version_variables() {
+    version="${1:1}"
+    export VERSION=$version
+    export BASE_DIR=$(realpath $(dirname $0)/../..)
+    export MSG="update to new release ${VERSION}"
+}
+
+checkout_main() {
+    # need these in order to commit
+    git config --global user.name "Gitlab action"
+    git config --global user.email "gitaction@axway.com"
+    git fetch
+    git checkout main
+}
+
+update_version_file() {
+    echo "Updating version file to version ${VERSION}"
+    echo ${VERSION} > ${BASE_DIR}/version
+}
+
+commit_promotion() {
+    echo "Committing the new promoted version to main"
+    cd ${BASE_DIR}
+    git add version
+    git commit -m "INT - ${MSG}"
+    git push origin main
+}
+
+main() {
+    check_required_param $1
+    if [ $? -eq 1 ]; then
+        echo "Promotion of release not completed. Missing parameter for release version (e.g. v1.2.3)"
+        echo "version file not updated. You can update it manually if you wish."
+        exit
+    fi
+
+    checkout_main
+
+    set_version_variables $1
+
+    update_version_file
+
+    commit_promotion
+}
+
+main $@
