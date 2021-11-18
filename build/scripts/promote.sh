4476edc7faf8e58ccff53749ec2f8ed828c9ee7d
dfeldick <dfeldick@axway.com>
2021-11-18 16:18:04 -0700

    INT - update release action and script

--- build/scripts/promote.sh
+++ build/scripts/promote.sh
@@ -15,9 +15,17 @@ check_required_param() {
 
 set_version_variables() {
     version="${1:1}"
-    export VERSION=$version
+
+    let MAJOR_VERSION=$(echo $version | cut -d. -f1)
+    let MINOR_VERSION=$(echo $version | cut -d. -f2)
+    let PATCH_VERSION=$(echo $version | cut -d. -f3)
+    let NEW_PATCH_VERSION=($PATCH_VERSION+1)
+
+    # right now, this only does patch versioning.
+    export NEW_VERSION="${MAJOR_VERSION}.${MINOR_VERSION}.${NEW_PATCH_VERSION}"
+
     export BASE_DIR=$(realpath $(dirname $0)/../..)
-    export MSG="update to new release ${VERSION}"
+    export MSG="update to new release ${NEW_VERSION}"
 }
 
 checkout_main() {
@@ -29,8 +37,8 @@ checkout_main() {
 }
 
 update_version_file() {
-    echo "Updating version file to version ${VERSION}"
-    echo ${VERSION} > ${BASE_DIR}/version
+    echo "Updating version file to version ${NEW_VERSION}"
+    echo ${NEW_VERSION} > ${BASE_DIR}/version
 }
 
 commit_promotion() {
