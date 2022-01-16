312ecb27a852e6d678f3e0b313c0db6cfa2d2a1a
Jason Collins <jcollins@axway.com>
2022-01-16 11:00:07 -0700

    APIGOV-20492-2 - add some logging

--- pkg/agent/teamcache.go
+++ pkg/agent/teamcache.go
@@ -40,6 +40,7 @@ func (j *centralTeamsCache) Execute() error {
 		if id, err := agent.teamMap.Get(team.Name); err != nil || id != team.ID {
 			err = agent.teamMap.Set(team.Name, team.ID)
 			if j.teamChannel != nil {
+				log.Tracef("sending %s (%) team to acl", team.Name, team.ID)
 				j.teamChannel <- team.ID
 			}
 		}
