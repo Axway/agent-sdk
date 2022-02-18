a65befe1a2fd7b2583f97efc8a7cc915d1d8b2a0
Jason Collins <jcollins@axway.com>
2022-02-18 09:28:52 -0700

    APIGOV-21283 - start adding tests

--- pkg/apic/subscription_test.go
+++ pkg/apic/subscription_test.go
@@ -6,6 +6,7 @@ import (
 	"testing"
 
 	"github.com/Axway/agent-sdk/pkg/api"
+	uc "github.com/Axway/agent-sdk/pkg/apic/unifiedcatalog/models"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -19,59 +20,53 @@ func TestUpdateProperties(t *testing.T) {
 	cs := subscription.(*CentralSubscription)
 	cs.apicClient = svcClient
 
-	// fail
-	mockHTTPClient.SetResponses([]api.MockResponse{
+	testCases := []struct {
+		name          string
+		filenames     []string
+		responseCodes []int
+		wantErr       bool
+	}{
 		{
-			RespCode: http.StatusBadRequest,
-		},
-	})
-	err := subscription.UpdateProperties("11111")
-	assert.NotNil(t, err)
-
-	// fail
-	mockHTTPClient.SetResponses([]api.MockResponse{
-		{
-			FileName: wd + "/testdata/catalogitemsubscriptiondefprofile.json",
-			RespCode: http.StatusOK,
+			name:          "Bad Response 1",
+			filenames:     []string{""},
+			responseCodes: []int{http.StatusBadRequest},
+			wantErr:       true,
 		},
 		{
-			RespCode: http.StatusBadRequest, // update status
-		},
-	})
-	err = subscription.UpdateProperties("11111")
-	assert.NotNil(t, err)
-
-	// failure
-	mockHTTPClient.SetResponses([]api.MockResponse{
-		{
-			FileName: wd + "/testdata/catalogitemsubscriptiondefprofile.json",
-			RespCode: http.StatusOK,
+			name:          "Bad Response 2",
+			filenames:     []string{wd + "/testdata/catalogitemsubscriptiondefprofile.json", ""},
+			responseCodes: []int{http.StatusOK, http.StatusBadRequest},
+			wantErr:       true,
 		},
 		{
-			RespCode: http.StatusOK, // update status
+			name:          "Bad Response 3",
+			filenames:     []string{wd + "/testdata/catalogitemsubscriptiondefprofile.json", "", ""},
+			responseCodes: []int{http.StatusOK, http.StatusOK, http.StatusBadRequest},
+			wantErr:       true,
 		},
 		{
-			RespCode: http.StatusBadRequest, // update property
+			name:          "Success",
+			filenames:     []string{wd + "/testdata/catalogitemsubscriptiondefprofile.json", "", ""},
+			responseCodes: []int{http.StatusOK, http.StatusOK, http.StatusOK},
+			wantErr:       false,
 		},
-	})
-	err = subscription.UpdateProperties("11111")
-	assert.NotNil(t, err)
-
-	// success
-	mockHTTPClient.SetResponses([]api.MockResponse{
-		{
-			FileName: wd + "/testdata/catalogitemsubscriptiondefprofile.json",
-			RespCode: http.StatusOK,
-		},
-		{
-			RespCode: http.StatusOK, // update status
-		},
-		{
-			RespCode: http.StatusOK, // update property
-		},
-	})
-	err = subscription.UpdateProperties("11111")
-	assert.Nil(t, err)
+	}
+	for _, tt := range testCases {
+		t.Run(tt.name, func(t *testing.T) {
+			mockResponses := make([]api.MockResponse, 0)
+			for i, code := range tt.responseCodes {
+				mockResponses = append(mockResponses,
+					api.MockResponse{
+						FileName: tt.filenames[i],
+						RespCode: code,
+					})
+			}
+			mockHTTPClient.SetResponses(mockResponses)
+			if err := subscription.UpdateProperties("11111"); (err != nil) != tt.wantErr {
+				t.Errorf("CentralSubscription.UpdatePropertyValues() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
 
 func TestUpdateState(t *testing.T) {
@@ -114,4 +109,55 @@ func TestGetters(t *testing.T) {
 	assert.Equal(t, SubscriptionApproved, subscription.GetState())
 	assert.Equal(t, "33333", subscription.GetPropertyValue("orgId"))
 	assert.Equal(t, "", subscription.GetPropertyValue("foo"))
+
+	accReq := createAccessRequestSubscription("access-request", string(AccessRequestProvisioning), "service-instance", map[string]interface{}{"orgId": "33333"})
+	assert.Equal(t, "bbunny", accReq.GetCreatedUserID())
+	assert.Equal(t, "access-request", accReq.GetID())
+	assert.Equal(t, "access-request", accReq.GetName())
+	assert.Equal(t, "service-instance", accReq.GetApicID())
+	assert.Equal(t, "2222", accReq.GetRemoteAPIID())
+	assert.Equal(t, "stage", accReq.GetRemoteAPIStage())
+	assert.Equal(t, "access-request", accReq.GetCatalogItemID())
+	assert.Equal(t, AccessRequestProvisioning, accReq.GetState())
+	assert.Equal(t, "33333", accReq.GetPropertyValue("orgId"))
+	assert.Equal(t, "", accReq.GetPropertyValue("foo"))
+}
+
+func TestCentralSubscription_UpdatePropertyValues(t *testing.T) {
+	type fields struct {
+		Subscription            Subscription
+		CatalogItemSubscription *uc.CatalogItemSubscription
+		ApicID                  string
+		RemoteAPIID             string
+		RemoteAPIStage          string
+		apicClient              *ServiceClient
+		RemoteAPIAttributes     map[string]string
+	}
+	type args struct {
+		values map[string]interface{}
+	}
+	tests := []struct {
+		name    string
+		fields  fields
+		args    args
+		wantErr bool
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			s := &CentralSubscription{
+				Subscription:            tt.fields.Subscription,
+				CatalogItemSubscription: tt.fields.CatalogItemSubscription,
+				ApicID:                  tt.fields.ApicID,
+				RemoteAPIID:             tt.fields.RemoteAPIID,
+				RemoteAPIStage:          tt.fields.RemoteAPIStage,
+				apicClient:              tt.fields.apicClient,
+				RemoteAPIAttributes:     tt.fields.RemoteAPIAttributes,
+			}
+			if err := s.UpdatePropertyValues(tt.args.values); (err != nil) != tt.wantErr {
+				t.Errorf("CentralSubscription.UpdatePropertyValues() error = %v, wantErr %v", err, tt.wantErr)
+			}
+		})
+	}
 }
