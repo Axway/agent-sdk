eaecda9247904fa62f6c505f29ac5b7b95b2fd49
epiasentin-axway <11756252+epiasentin-axway@users.noreply.github.com>
2022-02-18 17:33:25 +0100

    [STREAMS-2440] - add support of integer,number,object,array subscription property builder (#309)
    * STREAMS-2440 feat(subscriptionschema): Add support of integer, number, array and object for subscription property builder.
    
    - Add new interfaces for each subscription property builder
    - Update SubscriptionSchemaPropertyDefinition model to manage other types than string
    - Clean SubscriptionSchemaPropertyDefinition model (remove useless attributes)
    - Add parametrized unit tests
    - Update documentation to describe new builders
    
    * STREAMS-2440 chore(subscriptionschema): add missing comments for exported methods.
    
    Co-authored-by: Romain BISSEY <rbissey@axway.com>

--- pkg/apic/subscriptionschemapropertybuilder.go
+++ pkg/apic/subscriptionschemapropertybuilder.go
@@ -5,43 +5,113 @@ import (
 	"sort"
 )
 
-// SubscriptionPropertyBuilder - used to build a subscription schmea property
-type SubscriptionPropertyBuilder interface {
-	SetName(name string) SubscriptionPropertyBuilder
-	SetDescription(description string) SubscriptionPropertyBuilder
-	SetEnumValues(values []string) SubscriptionPropertyBuilder
-	SetSortEnumValues() SubscriptionPropertyBuilder
-	SetFirstEnumValue(value string) SubscriptionPropertyBuilder
-	AddEnumValue(value string) SubscriptionPropertyBuilder
-	SetRequired() SubscriptionPropertyBuilder
-	SetReadOnly() SubscriptionPropertyBuilder
-	SetHidden() SubscriptionPropertyBuilder
-	SetAPICRefField(field string) SubscriptionPropertyBuilder
-	IsString() SubscriptionPropertyBuilder
+// Supported data types
+const (
+	DataTypeString  = "string"
+	DataTypeNumber  = "number"
+	DataTypeInteger = "integer"
+	DataTypeArray   = "array"
+	DataTypeObject  = "object"
+)
+
+// PropertyBuilder - mandatory methods for all property builders
+type PropertyBuilder interface {
+	// Build - builds the property, this is called automatically by the schema builder
 	Build() (*SubscriptionSchemaPropertyDefinition, error)
 }
 
-// schemaProperty - holds all the info needed to create a subscrition schema property
+// SubscriptionPropertyBuilder - common methods related to subscription property builders
+type SubscriptionPropertyBuilder interface {
+	// SetName - sets the name of the property
+	SetName(name string) SubscriptionPropertyBuilder
+	// SetDescription - set the description of the property
+	SetDescription(description string) SubscriptionPropertyBuilder
+	// SetRequired - set the property as a required field in the schema
+	SetRequired() SubscriptionPropertyBuilder
+	// SetReadOnly - set the property as a read only property
+	SetReadOnly() SubscriptionPropertyBuilder
+	// SetHidden - set the property as a hidden property
+	SetHidden() SubscriptionPropertyBuilder
+	// SetAPICRefField - set the apic reference field for this property
+	SetAPICRefField(field string) SubscriptionPropertyBuilder
+	// IsString - Set the property to be of type string
+	IsString() StringPropertyBuilder
+	// IsInteger - Set the property to be of type integer
+	IsInteger() IntegerPropertyBuilder
+	// IsNumber - Set the property to be of type number
+	IsNumber() NumberPropertyBuilder
+	// IsArray - Set the property to be of type array
+	IsArray() ArrayPropertyBuilder
+	// IsObject - Set the property to be of type object
+	IsObject() ObjectPropertyBuilder
+	PropertyBuilder
+}
+
+// StringPropertyBuilder - specific methods related to the String property builders
+type StringPropertyBuilder interface {
+	// SetEnumValues - Set a list of valid values for the property
+	SetEnumValues(values []string) StringPropertyBuilder
+	// SetSortEnumValues - Sort the allowed values alphabetically in the schema
+	SetSortEnumValues() StringPropertyBuilder
+	// SetFirstEnumValue - Set the value that should appear first in the list
+	SetFirstEnumValue(value string) StringPropertyBuilder
+	// AddEnumValue - Add another value to the list of allowed values for the property
+	AddEnumValue(value string) StringPropertyBuilder
+	PropertyBuilder
+}
+
+// NumberPropertyBuilder - specific methods related to the Number property builders
+type NumberPropertyBuilder interface {
+	// SetMinValue - Set the minimum allowed number value
+	SetMinValue(min float64) NumberPropertyBuilder
+	// SetMaxValue - Set the maximum allowed number value
+	SetMaxValue(min float64) NumberPropertyBuilder
+	PropertyBuilder
+}
+
+// IntegerPropertyBuilder - specific methods related to the Integer property builders
+type IntegerPropertyBuilder interface {
+	// SetMinValue - Set the minimum allowed integer value
+	SetMinValue(min int64) IntegerPropertyBuilder
+	// SetMaxValue - Set the maximum allowed integer value
+	SetMaxValue(min int64) IntegerPropertyBuilder
+	PropertyBuilder
+}
+
+// ObjectPropertyBuilder - specific methods related to the Object property builders
+type ObjectPropertyBuilder interface {
+	// AddProperty - Add a property in the object property
+	AddProperty(property PropertyBuilder) ObjectPropertyBuilder
+	PropertyBuilder
+}
+
+// ArrayPropertyBuilder - specific methods related to the Array property builders
+type ArrayPropertyBuilder interface {
+	// AddItem - Add a item property in the array property
+	AddItem(item PropertyBuilder) ArrayPropertyBuilder
+	// SetMinItems - Set the minimum number of items in the array property
+	SetMinItems(min uint) ArrayPropertyBuilder
+	// SetMaxItems - Set the maximum number of items in the array property
+	SetMaxItems(max uint) ArrayPropertyBuilder
+	PropertyBuilder
+}
+
+// schemaProperty - holds basic info needed to create a subscription schema property
 type schemaProperty struct {
+	err          error
+	name         string
+	description  string
+	apicRefField string
+	required     bool
+	readOnly     bool
+	hidden       bool
+	dataType     string
 	SubscriptionPropertyBuilder
-	err            error
-	name           string
-	description    string
-	apicRefField   string
-	enums          []string
-	required       bool
-	readOnly       bool
-	hidden         bool
-	dataType       string
-	sortEnums      bool
-	firstEnumValue string
 }
 
 // NewSubscriptionSchemaPropertyBuilder - Creates a new subscription schema property builder
 func NewSubscriptionSchemaPropertyBuilder() SubscriptionPropertyBuilder {
-	return &schemaProperty{
-		enums: make([]string, 0),
-	}
+	return &schemaProperty{}
 }
 
 // SetName - sets the name of the property
@@ -53,52 +123,6 @@ func (p *schemaProperty) SetName(name string) SubscriptionPropertyBuilder {
 // SetDescription - set the description of the property
 func (p *schemaProperty) SetDescription(description string) SubscriptionPropertyBuilder {
 	p.description = description
-	return p
-}
-
-// SetEnumValues - add a list of enum values to the property
-func (p *schemaProperty) SetEnumValues(values []string) SubscriptionPropertyBuilder {
-	dict := make(map[string]bool, 0)
-
-	// use a temp map to filter out any duplicate values from the input
-	for _, value := range values {
-		if _, ok := dict[value]; !ok {
-			dict[value] = true
-			p.enums = append(p.enums, value)
-		}
-	}
-
-	return p
-}
-
-// SetSortEnumValues - indicates to sort the enums
-func (p *schemaProperty) SetSortEnumValues() SubscriptionPropertyBuilder {
-	p.sortEnums = true
-	return p
-}
-
-// SetFirstEnumValue - Sets a first item for enums. Only needed for sorted enums if you want a specific
-// item first in the list
-func (p *schemaProperty) SetFirstEnumValue(value string) SubscriptionPropertyBuilder {
-	p.firstEnumValue = value
-	return p
-}
-
-func (p *schemaProperty) enumContains(str string) bool {
-	for _, v := range p.enums {
-		if v == str {
-			return true
-		}
-	}
-
-	return false
-}
-
-// AddEnumValue - add a new value to the enum list
-func (p *schemaProperty) AddEnumValue(value string) SubscriptionPropertyBuilder {
-	if !p.enumContains(value) {
-		p.enums = append(p.enums, value)
-	}
 	return p
 }
 
@@ -126,17 +150,49 @@ func (p *schemaProperty) SetAPICRefField(field string) SubscriptionPropertyBuild
 	return p
 }
 
-// IsString - mark the datatype of the property as a string
-func (p *schemaProperty) IsString() SubscriptionPropertyBuilder {
-	if p.dataType != "" {
-		p.err = fmt.Errorf("The data type cannot be set to string, it is already set to %v", p.dataType)
-	} else {
-		p.dataType = "string"
+// IsString - Set the property to be of type string
+func (p *schemaProperty) IsString() StringPropertyBuilder {
+	p.dataType = DataTypeString
+	return &stringSchemaProperty{
+		schemaProperty: p,
 	}
-	return p
 }
 
-// Build - create the SubscriptionSchemaPropertyDefinition for use in the subscription schema builder
+// IsNumber - Set the property to be of type number
+func (p *schemaProperty) IsNumber() NumberPropertyBuilder {
+	p.dataType = DataTypeNumber
+	return &numberSchemaProperty{
+		schemaProperty: p,
+	}
+}
+
+// IsInteger - Set the property to be of type integer
+func (p *schemaProperty) IsInteger() IntegerPropertyBuilder {
+	p.dataType = DataTypeInteger
+	return &integerSchemaProperty{
+		numberSchemaProperty{
+			schemaProperty: p,
+		},
+	}
+}
+
+// IsArray - Set the property to be of type array
+func (p *schemaProperty) IsArray() ArrayPropertyBuilder {
+	p.dataType = DataTypeArray
+	return &arraySchemaProperty{
+		schemaProperty: p,
+	}
+}
+
+// IsObject - Set the property to be of type object
+func (p *schemaProperty) IsObject() ObjectPropertyBuilder {
+	p.dataType = DataTypeObject
+	return &objectSchemaProperty{
+		schemaProperty: p,
+	}
+}
+
+// Build - create a string SubscriptionSchemaPropertyDefinition for use in the subscription schema builder
 func (p *schemaProperty) Build() (*SubscriptionSchemaPropertyDefinition, error) {
 	if p.err != nil {
 		return nil, p.err
@@ -146,7 +202,89 @@ func (p *schemaProperty) Build() (*SubscriptionSchemaPropertyDefinition, error) 
 	}
 
 	if p.dataType == "" {
-		return nil, fmt.Errorf("Subscription schema property named %v must have a data type", p.name)
+		return nil, fmt.Errorf("Subscription schema property named %s must have a data type", p.name)
+	}
+
+	prop := &SubscriptionSchemaPropertyDefinition{
+		Name:        p.name,
+		Title:       p.name,
+		Type:        p.dataType,
+		Description: p.description,
+		APICRef:     p.apicRefField,
+		ReadOnly:    p.readOnly,
+		Required:    p.required,
+	}
+
+	if p.hidden {
+		prop.Format = "hidden"
+	}
+
+	return prop, nil
+}
+
+/**
+  string property datatype
+*/
+// stringSchemaProperty - adds specific info needed for a string schema property
+type stringSchemaProperty struct {
+	schemaProperty *schemaProperty
+	sortEnums      bool
+	firstEnumValue string
+	enums          []string
+	StringPropertyBuilder
+}
+
+// SetEnumValues - add a list of enum values to the property
+func (p *stringSchemaProperty) SetEnumValues(values []string) StringPropertyBuilder {
+	dict := make(map[string]bool, 0)
+
+	// use a temp map to filter out any duplicate values from the input
+	for _, value := range values {
+		if _, ok := dict[value]; !ok {
+			dict[value] = true
+			p.enums = append(p.enums, value)
+		}
+	}
+
+	return p
+}
+
+// SetSortEnumValues - indicates to sort the enums
+func (p *stringSchemaProperty) SetSortEnumValues() StringPropertyBuilder {
+	p.sortEnums = true
+	return p
+}
+
+// SetFirstEnumValue - Sets a first item for enums. Only needed for sorted enums if you want a specific
+// item first in the list
+func (p *stringSchemaProperty) SetFirstEnumValue(value string) StringPropertyBuilder {
+	p.firstEnumValue = value
+	return p
+}
+
+func (p *stringSchemaProperty) enumContains(str string) bool {
+	for _, v := range p.enums {
+		if v == str {
+			return true
+		}
+	}
+	return false
+}
+
+// AddEnumValue - Add another value to the list of allowed values for the property
+func (p *stringSchemaProperty) AddEnumValue(value string) StringPropertyBuilder {
+	if !p.enumContains(value) {
+		p.enums = append(p.enums, value)
+	}
+	return p
+}
+
+// Build - create a string SubscriptionSchemaPropertyDefinition for use in the subscription schema builder
+func (p *stringSchemaProperty) Build() (def *SubscriptionSchemaPropertyDefinition, err error) {
+
+	def, err = p.schemaProperty.Build()
+	if err != nil {
+		return
 	}
 
 	// sort if specified to do so
@@ -158,22 +296,173 @@ func (p *schemaProperty) Build() (*SubscriptionSchemaPropertyDefinition, error) 
 	if p.firstEnumValue != "" {
 		p.enums = append([]string{p.firstEnumValue}, p.enums...)
 	}
+	def.Enum = p.enums
+	return def, err
+}
 
-	prop := &SubscriptionSchemaPropertyDefinition{
-		Name:          p.name,
-		Type:          p.dataType,
-		Description:   p.description,
-		APICRef:       p.apicRefField,
-		ReadOnly:      p.readOnly,
-		Required:      p.required,
-		Enum:          p.enums,
-		SortEnums:     p.sortEnums,
-		FirstEnumItem: p.firstEnumValue,
+/**
+  number property datatype builder
+*/
+// numberSchemaProperty - adds specific info needed for a number schema property
+type numberSchemaProperty struct {
+	schemaProperty *schemaProperty
+	minValue       *float64 // We use a pointer to differentiate the "blank value" from a choosen 0 min value
+	maxValue       *float64 // We use a pointer to differentiate the "blank value" from a choosen 0 max value
+	SubscriptionPropertyBuilder
+}
+
+// SetMinValue - set the minimum allowed value
+func (p *numberSchemaProperty) SetMinValue(min float64) NumberPropertyBuilder {
+	p.minValue = &min
+	return p
+}
+
+// SetMaxValue - set the maximum allowed value
+func (p *numberSchemaProperty) SetMaxValue(max float64) NumberPropertyBuilder {
+	p.maxValue = &max
+	return p
+}
+
+// Build - create the SubscriptionSchemaPropertyDefinition for use in the subscription schema builder
+func (p *numberSchemaProperty) Build() (def *SubscriptionSchemaPropertyDefinition, err error) {
+	def, err = p.schemaProperty.Build()
+	if err != nil {
+		return
 	}
 
-	if p.hidden {
-		prop.Format = "hidden"
+	if p.minValue != nil && p.maxValue != nil && *p.minValue > *p.maxValue {
+		return nil, fmt.Errorf("Max value (%f) must be greater than min value (%f)", *p.maxValue, *p.minValue)
 	}
 
-	return prop, nil
+	def.Minimum = p.minValue
+	def.Maximum = p.maxValue
+	return def, err
+}
+
+/**
+  integer property datatype builder
+*/
+// integerSchemaProperty - adds specific info needed for an integer schema property
+type integerSchemaProperty struct {
+	numberSchemaProperty
+}
+
+// SetMinValue - set the minimum allowed value
+func (p *integerSchemaProperty) SetMinValue(min int64) IntegerPropertyBuilder {
+	minimum := float64(min)
+	p.minValue = &minimum
+	return p
+}
+
+// SetMaxValue - set the maximum allowed value
+func (p *integerSchemaProperty) SetMaxValue(max int64) IntegerPropertyBuilder {
+	maximum := float64(max)
+	p.maxValue = &maximum
+	return p
+}
+
+/**
+  array property datatype builder
+*/
+// arraySchemaProperty - adds specific info needed for an array schema property
+type arraySchemaProperty struct {
+	schemaProperty *schemaProperty
+	items          []SubscriptionSchemaPropertyDefinition
+	minItems       *uint
+	maxItems       *uint
+	SubscriptionPropertyBuilder
+}
+
+// AddItem - add an item in the array property
+func (p *arraySchemaProperty) AddItem(item PropertyBuilder) ArrayPropertyBuilder {
+	def, err := item.Build()
+	if err == nil {
+		p.items = append(p.items, *def)
+	} else {
+		p.schemaProperty.err = err
+	}
+	return p
+}
+
+// SetMinItems - set the minimum items in the property array
+func (p *arraySchemaProperty) SetMinItems(min uint) ArrayPropertyBuilder {
+	p.minItems = &min
+	return p
+}
+
+// SetMaxItems - set the maximum items in the property array
+func (p *arraySchemaProperty) SetMaxItems(max uint) ArrayPropertyBuilder {
+	if max < 1 {
+		p.schemaProperty.err = fmt.Errorf("The max array items must be greater than 0")
+	} else {
+		p.maxItems = &max
+	}
+	return p
+}
+
+// Build - create the SubscriptionSchemaPropertyDefinition for use in the subscription schema builder
+func (p *arraySchemaProperty) Build() (def *SubscriptionSchemaPropertyDefinition, err error) {
+	def, err = p.schemaProperty.Build()
+	if err != nil {
+		return
+	}
+
+	var anyOfItems *AnyOfSubscriptionSchemaPropertyDefinitions
+	if p.items != nil {
+		anyOfItems = &AnyOfSubscriptionSchemaPropertyDefinitions{p.items}
+	}
+
+	if p.minItems != nil && p.maxItems != nil && *p.minItems > *p.maxItems {
+		return nil, fmt.Errorf("Max array items (%d) must be greater than min array items (%d)", p.maxItems, p.minItems)
+	}
+
+	def.Items = anyOfItems
+	def.MinItems = p.minItems
+	def.MaxItems = p.maxItems
+	return def, err
+}
+
+/**
+  object property datatype builder
+*/
+// objectSchemaProperty - adds specific info needed for an object schema property
+type objectSchemaProperty struct {
+	schemaProperty *schemaProperty
+	properties     map[string]SubscriptionSchemaPropertyDefinition
+	SubscriptionPropertyBuilder
+}
+
+// AddProperty - Add a property in the object property
+func (p *objectSchemaProperty) AddProperty(property PropertyBuilder) ObjectPropertyBuilder {
+	def, err := property.Build()
+	if err == nil {
+		if p.properties == nil {
+			p.properties = make(map[string]SubscriptionSchemaPropertyDefinition, 0)
+		}
+		p.properties[def.Name] = *def
+	} else {
+		p.schemaProperty.err = err
+	}
+	return p
+}
+
+// Build - create the SubscriptionSchemaPropertyDefinition for use in the subscription schema builder
+func (p *objectSchemaProperty) Build() (def *SubscriptionSchemaPropertyDefinition, err error) {
+	def, err = p.schemaProperty.Build()
+	if err != nil {
+		return
+	}
+
+	var requiredProperties []string
+	if p.properties != nil {
+		for _, property := range p.properties {
+			if property.Required {
+				requiredProperties = append(requiredProperties, property.Name)
+			}
+		}
+	}
+
+	def.Properties = p.properties
+	def.RequiredProperties = requiredProperties
+	return def, err
 }
--- pkg/apic/subscriptionschemapropertybuilder_test.go
+++ pkg/apic/subscriptionschemapropertybuilder_test.go
@@ -1,6 +1,7 @@
 package apic
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -17,7 +18,7 @@ func TestNewSubscriptionSchemaPropertyBuilder(t *testing.T) {
 	assert.Empty(t, schemaProperty.name)
 	assert.Empty(t, schemaProperty.description)
 	assert.Empty(t, schemaProperty.apicRefField)
-	assert.Len(t, schemaProperty.enums, 0)
+	//assert.Len(t, schemaProperty.enums, 0)
 	assert.False(t, schemaProperty.required)
 	assert.False(t, schemaProperty.readOnly)
 	assert.Empty(t, schemaProperty.dataType)
@@ -39,14 +40,13 @@ func TestSubscriptionSchemaPropertyBuilderSetters(t *testing.T) {
 	assert.Nil(t, prop)
 
 	// Datatype twice
-	prop, err = NewSubscriptionSchemaPropertyBuilder().
-		SetName("name").
-		IsString().
-		IsString().
-		Build()
+	//prop, err = NewSubscriptionSchemaPropertyBuilder().
+	//	SetName("name").
+	//	IsString().
+	//	Build()
 
-	assert.NotNil(t, err)
-	assert.Nil(t, prop)
+	//assert.NotNil(t, err)
+	//assert.Nil(t, prop)
 
 	// good path, no enums
 	prop, err = NewSubscriptionSchemaPropertyBuilder().
@@ -105,8 +105,6 @@ func TestSubscriptionSchemaPropertyBuilderSetters(t *testing.T) {
 	assert.False(t, prop.ReadOnly)
 	assert.Equal(t, prop.Format, "")
 	assert.Equal(t, "", prop.APICRef)
-	assert.False(t, prop.SortEnums)
-	assert.Equal(t, "", prop.FirstEnumItem)
 
 	// good path, sort enums & add first item
 	prop, err = NewSubscriptionSchemaPropertyBuilder().
@@ -128,8 +126,292 @@ func TestSubscriptionSchemaPropertyBuilderSetters(t *testing.T) {
 	assert.False(t, prop.ReadOnly)
 	assert.Equal(t, prop.Format, "")
 	assert.Equal(t, "", prop.APICRef)
-	assert.True(t, prop.SortEnums)
-	assert.Equal(t, "xxx", prop.FirstEnumItem)
 	assert.Equal(t, "xxx", prop.Enum[0])
 	assert.Equal(t, "a", prop.Enum[1])
+}
+
+func getFloat64Pointer(value float64) *float64 {
+	return &value
+}
+
+func getUintPointer(value uint) *uint {
+	return &value
+}
+
+func Test_SubscriptionPropertyBuilder_Build_with_valid_values(t *testing.T) {
+	tests := []struct {
+		name        string
+		builder     PropertyBuilder
+		expectedDef SubscriptionSchemaPropertyDefinition
+	}{
+		{"Minimal String property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				IsString(),
+			SubscriptionSchemaPropertyDefinition{
+				Name:  "TheName",
+				Title: "TheName",
+				Type:  DataTypeString,
+			}},
+		{"Full String property with unsorted enum and first value",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				SetDescription("TheDescription").
+				SetRequired().
+				SetHidden().
+				SetReadOnly().
+				SetAPICRefField("APICRefField").
+				IsString().
+				SetEnumValues([]string{"c", "a", "b"}).
+				AddEnumValue("addedValue").
+				SetFirstEnumValue("firstValue"),
+			SubscriptionSchemaPropertyDefinition{
+				Name:        "TheName",
+				Title:       "TheName",
+				Description: "TheDescription",
+				Required:    true,
+				Format:      "hidden",
+				ReadOnly:    true,
+				APICRef:     "APICRefField",
+				Type:        DataTypeString,
+				Enum:        []string{"firstValue", "c", "a", "b", "addedValue"},
+			}},
+		{"Full String property with sorted enum and first value",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				SetDescription("TheDescription").
+				SetRequired().
+				SetHidden().
+				SetReadOnly().
+				SetAPICRefField("APICRefField").
+				IsString().
+				SetEnumValues([]string{"c", "a", "b"}).
+				AddEnumValue("addedValue").
+				SetFirstEnumValue("firstValue").
+				SetSortEnumValues(),
+			SubscriptionSchemaPropertyDefinition{
+				Name:        "TheName",
+				Title:       "TheName",
+				Description: "TheDescription",
+				Required:    true,
+				Format:      "hidden",
+				ReadOnly:    true,
+				APICRef:     "APICRefField",
+				Type:        DataTypeString,
+				Enum:        []string{"firstValue", "a", "addedValue", "b", "c"},
+			}},
+		{"Minimal Number property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				IsNumber(),
+			SubscriptionSchemaPropertyDefinition{
+				Name:  "TheName",
+				Title: "TheName",
+				Type:  DataTypeNumber,
+			}},
+		{"Full Number property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				SetDescription("TheDescription").
+				SetRequired().
+				SetHidden().
+				SetReadOnly().
+				SetAPICRefField("APICRefField").
+				IsNumber().
+				SetMinValue(0.0).
+				SetMaxValue(100.5),
+			SubscriptionSchemaPropertyDefinition{
+				Name:        "TheName",
+				Title:       "TheName",
+				Description: "TheDescription",
+				Required:    true,
+				Format:      "hidden",
+				ReadOnly:    true,
+				APICRef:     "APICRefField",
+				Type:        DataTypeNumber,
+				Minimum:     getFloat64Pointer(0.0),
+				Maximum:     getFloat64Pointer(100.5),
+			}},
+		{"Minimal Integer property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				IsInteger(),
+			SubscriptionSchemaPropertyDefinition{
+				Name:  "TheName",
+				Title: "TheName",
+				Type:  DataTypeInteger,
+			}},
+		{"Full Integer property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				SetDescription("TheDescription").
+				SetRequired().
+				SetHidden().
+				SetReadOnly().
+				SetAPICRefField("APICRefField").
+				IsInteger().
+				SetMinValue(0).
+				SetMaxValue(100),
+			SubscriptionSchemaPropertyDefinition{
+				Name:        "TheName",
+				Title:       "TheName",
+				Description: "TheDescription",
+				Required:    true,
+				Format:      "hidden",
+				ReadOnly:    true,
+				APICRef:     "APICRefField",
+				Type:        DataTypeInteger,
+				Minimum:     getFloat64Pointer(0),
+				Maximum:     getFloat64Pointer(100),
+			}},
+		{"Minimal Array property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				IsArray(),
+			SubscriptionSchemaPropertyDefinition{
+				Name:  "TheName",
+				Title: "TheName",
+				Type:  DataTypeArray,
+			}},
+		{"Full Array property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				SetDescription("TheDescription").
+				SetRequired().
+				SetHidden().
+				SetReadOnly().
+				SetAPICRefField("APICRefField").
+				IsArray().
+				AddItem(NewSubscriptionSchemaPropertyBuilder().
+					SetName("ItemName").
+					IsString()).
+				SetMinItems(0).
+				SetMaxItems(1),
+			SubscriptionSchemaPropertyDefinition{
+				Name:        "TheName",
+				Title:       "TheName",
+				Description: "TheDescription",
+				Required:    true,
+				Format:      "hidden",
+				ReadOnly:    true,
+				APICRef:     "APICRefField",
+				Type:        DataTypeArray,
+				Items: &AnyOfSubscriptionSchemaPropertyDefinitions{
+					AnyOf: []SubscriptionSchemaPropertyDefinition{
+						{
+							Name:  "ItemName",
+							Title: "ItemName",
+							Type:  DataTypeString,
+						},
+					},
+				},
+				MinItems: getUintPointer(0),
+				MaxItems: getUintPointer(1),
+			}},
+		{"Minimal Object property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				IsObject(),
+			SubscriptionSchemaPropertyDefinition{
+				Name:  "TheName",
+				Title: "TheName",
+				Type:  DataTypeObject,
+			}},
+		{"Full Object property",
+			NewSubscriptionSchemaPropertyBuilder().
+				SetName("TheName").
+				SetDescription("TheDescription").
+				SetRequired().
+				SetHidden().
+				SetReadOnly().
+				SetAPICRefField("APICRefField").
+				IsObject().
+				AddProperty(NewSubscriptionSchemaPropertyBuilder().
+					SetName("PropertyName").
+					SetRequired().
+					IsString()),
+			SubscriptionSchemaPropertyDefinition{
+				Name:        "TheName",
+				Title:       "TheName",
+				Description: "TheDescription",
+				Required:    true,
+				Format:      "hidden",
+				ReadOnly:    true,
+				APICRef:     "APICRefField",
+				Type:        DataTypeObject,
+				Properties: map[string]SubscriptionSchemaPropertyDefinition{
+					"PropertyName": {
+						Name:     "PropertyName",
+						Title:    "PropertyName",
+						Type:     DataTypeString,
+						Required: true,
+					},
+				},
+				RequiredProperties: []string{
+					"PropertyName",
+				},
+			}},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			def, err := tt.builder.Build()
+			assert.Nil(t, err)
+			assert.Equal(t, tt.expectedDef, *def)
+		})
+	}
+}
+
+func Test_SubscriptionPropertyBuilder_Build_with_error(t *testing.T) {
+	tests := []struct {
+		name         string
+		builder      PropertyBuilder
+		errorPattern string
+	}{
+		{"String property without name", NewSubscriptionSchemaPropertyBuilder().
+			IsString(), "without a name"},
+		{"Number property without name", NewSubscriptionSchemaPropertyBuilder().
+			IsNumber(), "without a name"},
+		{"Number property with min greater than max", NewSubscriptionSchemaPropertyBuilder().
+			SetName("aNumber").
+			IsNumber().
+			SetMinValue(2).
+			SetMaxValue(1), "greater than"},
+		{"Integer property without name", NewSubscriptionSchemaPropertyBuilder().
+			IsInteger(),
+			"without a name"},
+		{"Integer property with min greater than max", NewSubscriptionSchemaPropertyBuilder().
+			SetName("anInteger").
+			IsInteger().
+			SetMinValue(2).
+			SetMaxValue(1), "greater than"},
+		{"Array property without name", NewSubscriptionSchemaPropertyBuilder().
+			IsArray(), "without a name"},
+		{"Array property with min items greater than max items", NewSubscriptionSchemaPropertyBuilder().
+			SetName("anArray").
+			IsArray().
+			SetMinItems(2).
+			SetMaxItems(1), "greater than"},
+		{"Array property with wrong max items", NewSubscriptionSchemaPropertyBuilder().
+			SetName("anArray").
+			IsArray().
+			SetMaxItems(0), "greater than 0"},
+		{"Array property with error on added item", NewSubscriptionSchemaPropertyBuilder().
+			SetName("anArray").
+			IsArray().
+			AddItem(NewSubscriptionSchemaPropertyBuilder()), "without a name"},
+		{"Object property without name", NewSubscriptionSchemaPropertyBuilder().
+			IsObject(), "without a name"},
+		{"Object property with error on added property", NewSubscriptionSchemaPropertyBuilder().
+			SetName("anObject").
+			IsObject().
+			AddProperty(NewSubscriptionSchemaPropertyBuilder()), "without a name"},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			prop, err := tt.builder.Build()
+			assert.NotNil(t, err)
+			assert.True(t, strings.Contains(err.Error(), tt.errorPattern))
+			assert.Nil(t, prop)
+		})
+	}
 }
