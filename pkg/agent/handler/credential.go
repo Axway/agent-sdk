cfa2bf322f7fdce927440fafad8e54890fd2dfc0
Jason Collins <jcollins@axway.com>
2022-03-10 10:24:29 -0700

    APIGOV-21858 - update

--- pkg/agent/handler/credential.go
+++ pkg/agent/handler/credential.go
@@ -97,12 +97,12 @@ func (h *credentials) Handle(action proto.Event_Type, meta *proto.EventMeta, res
 		status, credentialData = h.prov.CredentialProvision(creds)
 		sec := cApp.Spec.Security
 		enc, err := newEncryptor(sec.EncryptionKey, sec.EncryptionAlgorithm, sec.EncryptionHash)
-		if err == nil {
+		if err != nil {
+			status = prov.NewRequestStatusBuilder().SetMessage(fmt.Sprintf("error encrypting credential: %s", err.Error())).Failed()
+		} else {
 			if scheamProps, ok := crd.Spec.Provision.Schema["properties"]; ok {
 				cr.Data = h.encrypt(enc, scheamProps.(map[string]interface{}), credentialData.GetData())
 			}
-		} else {
-			status = prov.NewRequestStatusBuilder().SetMessage(fmt.Sprintf("error encrypting credential: %s", err.Error())).Failed()
 		}
 
 		cr.Status = prov.NewStatusReason(status)
