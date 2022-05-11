e6a7d6d469815cf5fcc88c560a95f706fa9c8e26
Jason Collins <jcollins@axway.com>
2022-05-11 13:32:02 -0700

    STREAMS-2712 - fix lint

--- pkg/util/util.go
+++ pkg/util/util.go
@@ -300,11 +300,13 @@ func MapStringInterfaceToStringString(data map[string]interface{}) map[string]st
 	return newData
 }
 
+// ConvertStringToUint -
 func ConvertStringToUint(val string) uint64 {
 	ret, _ := strconv.ParseUint(val, 10, 64)
 	return ret
 }
 
+// ConvertUnitToString -
 func ConvertUnitToString(val uint64) string {
 	return strconv.FormatUint(val, 10)
 }
