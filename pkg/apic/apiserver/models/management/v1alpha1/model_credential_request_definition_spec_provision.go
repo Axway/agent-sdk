89482cb376ac17c10f5dfb1d09be1d60e004eeb6
Jason Collins <jcollins@axway.com>
2022-08-31 15:57:08 -0700

    APIGOV-23492 - codegen updates for crd policy

--- pkg/apic/apiserver/models/management/v1alpha1/model_credential_request_definition_spec_provision.go
+++ pkg/apic/apiserver/models/management/v1alpha1/model_credential_request_definition_spec_provision.go
@@ -5,5 +5,6 @@ package management
 // CredentialRequestDefinitionSpecProvision struct for CredentialRequestDefinitionSpecProvision
 type CredentialRequestDefinitionSpecProvision struct {
 	// JSON Schema draft \\#7 for defining the AccessRequest properties needed to get access to an APIServiceInstance.
-	Schema map[string]interface{} `json:"schema"`
+	Schema   map[string]interface{}                           `json:"schema"`
+	Policies CredentialRequestDefinitionSpecProvisionPolicies `json:"policies,omitempty"`
 }
