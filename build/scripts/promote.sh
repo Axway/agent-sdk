e7fc96e7f3acc43ad0cee1d545de1a77a94a0891
dfeldick <dfeldick@axway.com>
2022-09-09 15:20:42 -0700

    APIGOV-dale - a

--- build/scripts/promote.sh
+++ build/scripts/promote.sh
@@ -60,7 +60,7 @@ commit_promotion() {
 }
 
 main() {
-    # check_required_param $1
+    ## check_required_param $1
     # if [ $? -eq 1 ]; then
     #     echo "Promotion of release not completed. Missing parameter for release version (e.g. v1.2.3)"
     #     echo "version file not updated. You can update it manually if you wish."
