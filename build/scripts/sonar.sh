4a4e66cdcf1c49774f3a32528631d6eb1e836400
dfeldick <dfeldick@axway.com>
2023-01-09 10:43:30 -0700

    INT - cleanup obsolete sonar preview

--- build/scripts/sonar.sh
+++ build/scripts/sonar.sh
@@ -17,27 +17,4 @@ sonar-scanner -X \
     -Dsonar.issuesReport.console.enable=true \
     -Dsonar.report.export.path=sonar-report.json
 
-if [ "${MODE}" = "preview" ]; then
-    echo "All reported issues:" && cat .scannerwork/sonar-report.json | jq ".issues"
-
-    ISSUES=$(cat .scannerwork/sonar-report.json | jq '.issues | .[] | select(.severity=="BLOCKER")' | jq -s length)
-    echo "BLOCKING Issues: ${ISSUES}"
-
-    ISSUES=$(cat .scannerwork/sonar-report.json | jq '.issues | .[] | select(.severity=="CRITICAL")' | jq -s length)
-    echo "CRITICAL Issues: ${ISSUES}"
-
-    ISSUES=$(cat .scannerwork/sonar-report.json | jq '.issues | .[] | select(.severity=="MAJOR")' | jq -s length)
-    echo "MAJOR Issues: ${ISSUES}"
-
-    ISSUES=$(cat .scannerwork/sonar-report.json | jq '.issues | .[] | select(.severity=="MINOR")' | jq -s length)
-    echo "MINOR Issues: ${ISSUES}"
-
-    HIGH_SEV_ISSUES=$(cat .scannerwork/sonar-report.json | jq '.issues | .[] | (select(.severity=="MAJOR"),select(.severity=="BLOCKER"),select(.severity=="CRITICAL"))' | jq -s length)
-    echo "All issues with a severity of MAJOR or higher: ${HIGH_SEV_ISSUES}"
-
-    if [ ${HIGH_SEV_ISSUES} -gt 0 ]; then
-        exit 1
-    fi
-fi
-
 exit 0
