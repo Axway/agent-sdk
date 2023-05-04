e95e4c33f8c36b2ac20053bb571135ad3bde8541
Vivek Singh Chauhan <vchauhan@axway.com>
2023-05-04 11:24:56 -0700

    APIGOV-25066 - updates to generate events in agent service

--- pkg/transaction/metric/metricevent.go
+++ pkg/transaction/metric/metricevent.go
@@ -45,37 +45,6 @@ func (c *CondorMetricEvent) CreateEvent() (beatPub.Event, error) {
 	return c.CreateEventWithContext(context.Background())
 }
 
-// // CreateEvent - creates the beat event to add to the batch
-// func (c *CondorMetricEvent) CreateEventWithContext(ctx context.Context) (beatPub.Event, error) {
-// 	c.Fields[traceability.FlowHeader] = metricFlow
-
-// 	// convert the CondorMetricEvent to json then to map[string]interface{}
-// 	cmeJSON, err := json.Marshal(c)
-// 	if err != nil {
-// 		return beatPub.Event{}, err
-// 	}
-
-// 	var fieldsData map[string]interface{}
-// 	err = json.Unmarshal(cmeJSON, &fieldsData)
-// 	if err != nil {
-// 		return beatPub.Event{}, err
-// 	}
-
-// 	beatEnv := beatPub.Event{
-// 		Content: beat.Event{
-// 			Timestamp: c.Timestamp,
-// 			Meta: map[string]interface{}{
-// 				metricKey:          c.ID,
-// 				sampling.SampleKey: true, // All metric events should be sent
-// 			},
-// 			Fields: fieldsData,
-// 		},
-// 		Flags: beatPub.GuaranteedSend,
-// 	}
-// 	log.Tracef("Created Metric Event: %+v", beatEnv)
-// 	return beatEnv, nil
-// }
-
 // CreateEvent - creates the beat event to add to the batch
 func (c *CondorMetricEvent) CreateEventWithContext(ctx context.Context) (beatPub.Event, error) {
 	if !skipAuthTokenField(ctx) {
@@ -141,40 +110,3 @@ func getBoolValueFromCtx(ctx context.Context, key string) bool {
 func skipAuthTokenField(ctx context.Context) bool {
 	return getBoolValueFromCtx(ctx, "skipAuthHeader")
 }
-
-// // CreateEvent - creates the beat event to add to the batch
-// func (c *CondorMetricEvent) CreateEvent() (beatPub.Event, error) {
-// 	// Get the event token
-// 	token, err := agent.GetCentralAuthToken()
-// 	if err != nil {
-// 		return beatPub.Event{}, err
-// 	}
-// 	c.Fields["token"] = token
-// 	c.Fields[traceability.FlowHeader] = metricFlow
-
-// 	// convert the CondorMetricEvent to json then to map[string]interface{}
-// 	cmeJSON, err := json.Marshal(c)
-// 	if err != nil {
-// 		return beatPub.Event{}, err
-// 	}
-
-// 	var fieldsData map[string]interface{}
-// 	err = json.Unmarshal(cmeJSON, &fieldsData)
-// 	if err != nil {
-// 		return beatPub.Event{}, err
-// 	}
-
-// 	beatEnv := beatPub.Event{
-// 		Content: beat.Event{
-// 			Timestamp: c.Timestamp,
-// 			Meta: map[string]interface{}{
-// 				metricKey:          c.ID,
-// 				sampling.SampleKey: true, // All metric events should be sent
-// 			},
-// 			Fields: fieldsData,
-// 		},
-// 		Flags: beatPub.GuaranteedSend,
-// 	}
-// 	log.Tracef("Created Metric Event: %+v", beatEnv)
-// 	return beatEnv, nil
-// }
