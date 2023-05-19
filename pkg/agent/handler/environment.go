eebd4999e22bc3936a16f3a9f2e31f407f21bcbe
sbolosan <sbolosan@axway.com>
2023-05-19 08:06:20 -0700

    APIGOV-25482 - update based on PR comment

--- pkg/agent/handler/environment.go
+++ pkg/agent/handler/environment.go
@@ -25,24 +25,27 @@ func NewEnvironmentHandler(agentCacheManager agentcache.Manager, credentialConfi
 }
 
 func (c *environmentHandler) Handle(ctx context.Context, meta *proto.EventMeta, resource *v1.ResourceInstance) error {
-	action := GetActionFromContext(ctx)
 	if resource.Kind != environment.EnvironmentGVK().Kind {
 		return nil
 	}
 
-	if action == proto.Event_SUBRESOURCEUPDATED && meta.Subresource == management.EnvironmentPoliciesSubResourceName {
-		log := getLoggerFromContext(ctx).WithComponent("environmentHandler")
-		env := &management.Environment{}
-		err := env.FromInstance(resource)
-		if err != nil {
-			log.WithError(err).Error("could not handle access request")
-			return nil
-		}
-
-		// Set up credential config from environment resource policies
-		c.credentialConfig.SetShouldDeprovisionExpired(env.Policies.Credentials.Expiry.Action == "deprovision")
-		c.credentialConfig.SetExpirationDays(int(env.Policies.Credentials.Expiry.Period))
+	// verify that action is subresource updated and meta subsresource is environment policy
+	action := GetActionFromContext(ctx)
+	if action != proto.Event_SUBRESOURCEUPDATED && meta.Subresource != management.EnvironmentPoliciesSubResourceName {
+		return nil
 	}
+
+	log := getLoggerFromContext(ctx).WithComponent("environmentHandler")
+	env := &management.Environment{}
+	err := env.FromInstance(resource)
+	if err != nil {
+		log.WithError(err).Error("could not handle access request")
+		return nil
+	}
+
+	// Set up credential config from environment resource policies
+	c.credentialConfig.SetShouldDeprovisionExpired(env.Policies.Credentials.Expiry.Action == "deprovision")
+	c.credentialConfig.SetExpirationDays(int(env.Policies.Credentials.Expiry.Period))
 
 	return nil
 }
