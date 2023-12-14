b091d412c8b029c95f4f96d9a93d37b30ffeb4af
Vivek Singh Chauhan <vchauhan@axway.com>
2023-12-13 19:38:19 -0700

    APIGOV-26876 - set the cacheUpdateTime in initCache

--- pkg/agent/eventsync.go
+++ pkg/agent/eventsync.go
@@ -137,6 +137,16 @@ func (es *EventSync) initCache() error {
 		return err
 	}
 	agent.cacheManager.SaveCache()
+
+	agentInstance := agent.agentResourceManager.GetAgentResource()
+
+	// add 7 days to the current date for the next rebuild cache
+	nextCacheUpdateTime := time.Now().Add(7 * 24 * time.Hour)
+
+	// persist cacheUpdateTime
+	util.SetAgentDetailsKey(agentInstance, "cacheUpdateTime", strconv.FormatInt(nextCacheUpdateTime.UnixNano(), 10))
+	agent.apicClient.CreateSubResource(agentInstance.ResourceMeta, util.GetSubResourceDetails(agentInstance))
+	logger.Tracef("setting next cache update time to - %s", time.Unix(0, nextCacheUpdateTime.UnixNano()).Format("2006-01-02 15:04:05.000000"))
 	return nil
 }
 
@@ -151,16 +161,6 @@ func (es *EventSync) RebuildCache() {
 	if err := es.initCache(); err != nil {
 		logger.WithError(err).Error("failed to rebuild cache")
 	}
-
-	agentInstance := agent.agentResourceManager.GetAgentResource()
-
-	// add 7 days to the current date for the next rebuild cache
-	nextCacheUpdateTime := time.Now().Add(7 * 24 * time.Hour)
-
-	// persist cacheUpdateTime
-	util.SetAgentDetailsKey(agentInstance, "cacheUpdateTime", strconv.FormatInt(nextCacheUpdateTime.UnixNano(), 10))
-	agent.apicClient.CreateSubResource(agentInstance.ResourceMeta, util.GetSubResourceDetails(agentInstance))
-	logger.Tracef("setting next cache update time to - %s", time.Unix(0, nextCacheUpdateTime.UnixNano()).Format("2006-01-02 15:04:05.000000"))
 }
 
 func (es *EventSync) startCentralEventProcessor() error {
