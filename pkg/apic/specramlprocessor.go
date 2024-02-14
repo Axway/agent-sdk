5e0f1164702ededf37f79cb014ab0a109b7d948c
Dragos Gabriel Ghinea <142506926+dgghinea@users.noreply.github.com>
2024-02-14 19:45:55 +0200

    APIGOV-27134 RAML spec support (#729)
    * APIGOV-27134 regenerate resources
    
    * APIGOV-27134 - raml init
    
    * APIGOV-27134 - updates
    
    * APIGOV-27134 - almost fix tests
    
    * APIGOV-27134 - fix test
    
    * Update index.md for APIGOV-26592 (#727)
    
    Update sampling percentage description
    
    * APIGOV-27134 - move files to same pkg
    
    * APIGOV-27134 - fix float interface version values
    
    * Revert "APIGOV-27134 regenerate resources"
    
    This reverts commit a32190d3d782fe3d008542334a7041799eb6abdc.
    
    * APIGOV-27134 - rename file to match others
    
    * Squashed commit of the following:
    
    commit 571e8ea24b6627072411dfa8c4b981369413cc0c
    Author: Jason Collins <47123298+jcollins-axway@users.noreply.github.com>
    Date:   Mon Feb 5 13:02:50 2024 -0700
    
        add test opt (#732)
    
    commit c740834a2348712847cc6b550accadfc5676e2c6
    Author: sbolosan <47189541+sbolosan@users.noreply.github.com>
    Date:   Fri Feb 2 12:02:32 2024 -0700
    
        Revert "APIGOV-27134 regenerate resources (#728)" (#730)
    
        This reverts commit 020b2d8b2f491f08b15a20615c038171b67fcae0.
    
    commit 020b2d8b2f491f08b15a20615c038171b67fcae0
    Author: Alin Rosca <137875910+alrosca@users.noreply.github.com>
    Date:   Fri Feb 2 20:20:58 2024 +0200
    
        APIGOV-27134 regenerate resources (#728)
    
        * APIGOV-27134 regenerate resources
    
        * resources_updates - ignore _embedded
    
        * resources_updates - fix test
    
        ---------
    
        Co-authored-by: sbolosan <sbolosan@axway.com>
    
    commit 241f603adf1d09078373d337700116062ed4d5d7
    Author: Dragos Gabriel Ghinea <142506926+dgghinea@users.noreply.github.com>
    Date:   Thu Feb 1 09:48:15 2024 +0200
    
        APIGOV-27100 - SDK float Sampling percentage changes (#723)
    
        * APIGOV-27100 - change sample percentage to float
    
        * APIGOV-26588 add graphQL api type (#721)
    
        * APIGOV-26893 - new usage report generation logic (#716)
    
        * APIGOV-26893 - new usage report generation logic
    
        * APIGOV-26893 - fix logic + tests
    
        * undo offline report aggregation + add tests (tests still need additional work)
    
        * fix aggregate report test
    
        * use better test
    
        * remove unused code
    
        * APIGOV-27099 - Update status and x-agent-details subreource during agent status updates (#722)
    
        * APIGOV-27100 - small optimization (avoid calculating shouldSample limit on every step)
    
        * APIGOV-27100 - fix default config
    
        * APIGOV-27100 - add comment
    
        ---------
    
        Co-authored-by: Alin Rosca <137875910+alrosca@users.noreply.github.com>
        Co-authored-by: Vivek Singh Chauhan <vchauhan@axway.com>
        Co-authored-by: Jason Collins <47123298+jcollins-axway@users.noreply.github.com>
    
    commit 81cda908dc21a6ff3d9a5177435c52c47cf43de5
    Author: Dragos Gabriel Ghinea <142506926+dgghinea@users.noreply.github.com>
    Date:   Tue Jan 30 09:34:02 2024 +0200
    
        Update index.md for APIGOV-26592 (#727)
    
        Update sampling percentage description
    
    * APIGOV-27134 - fixes
    
    ---------
    
    Co-authored-by: Alin Rosca <acrosca@axway.com>

---
+++ pkg/apic/specramlprocessor.go
@@ -0,0 +1,144 @@
+package apic
+
+import (
+	"fmt"
+	"net"
+	"net/url"
+	"strconv"
+	"strings"
+)
+
+type ramlProcessor struct {
+	ramlDef map[string]interface{}
+	spec    []byte
+}
+
+func newRamlProcessor(ramlDef map[string]interface{}, spec []byte) *ramlProcessor {
+	return &ramlProcessor{ramlDef: ramlDef, spec: spec}
+}
+
+func (p *ramlProcessor) GetResourceType() string {
+	return Raml
+}
+
+func (p *ramlProcessor) GetVersion() string {
+	if version := p.ramlDef["version"]; version != nil {
+		switch v := version.(type) {
+		// yaml Unmarshalling converts the underlying interface values which can be floats like 2.0 to 2
+		case float64:
+			if float64(int(v)) == v {
+				return fmt.Sprintf("%v.0", v)
+			}
+			return fmt.Sprintf("%v", v)
+		default:
+			return fmt.Sprintf("%v", v)
+		}
+	}
+	return ""
+}
+
+func (p *ramlProcessor) GetDescription() string {
+	if description := p.ramlDef["description"]; description != nil {
+		return fmt.Sprintf("%v", description)
+	}
+	return ""
+}
+
+func (p *ramlProcessor) GetEndpoints() ([]EndpointDefinition, error) {
+	baseUri := p.ramlDef["baseUri"]
+	if baseUri == nil {
+		return nil, fmt.Errorf("No baseUri provided")
+	}
+
+	if params := p.ramlDef["baseUriParameters"]; params != nil {
+		return nil, fmt.Errorf("Not implemented error")
+	}
+
+	return p.uriToEndpoints(baseUri.(string), p.getProtocols())
+}
+
+func (p *ramlProcessor) GetSpecBytes() []byte {
+	return p.spec
+}
+
+func (p *ramlProcessor) getProtocols() []string {
+	if protocols := p.ramlDef["protocols"]; protocols != nil {
+		// in case [HTTP, HTTPS] is provided
+		if ramlProtocols, ok := protocols.([]interface{}); ok {
+			return validateRamlProtocols(ramlProtocols)
+		}
+		// in case just HTTP is provided
+		if ramlProtocols, ok := protocols.(string); ok {
+			return validateRamlProtocols([]interface{}{ramlProtocols})
+		}
+	}
+	return nil
+}
+
+func (p *ramlProcessor) uriToEndpoints(uri string, protocols []string) ([]EndpointDefinition, error) {
+	parseURL, err := url.Parse(uri)
+	if err != nil {
+		return nil, err
+	}
+	if !strings.HasPrefix(strings.ToLower(parseURL.Scheme), "http") {
+		return p.uriToEndpoints("https://"+uri, protocols)
+	}
+	endpoints := []EndpointDefinition{}
+	ep := EndpointDefinition{}
+	// currently accepting only version as a dynamic value to the endpoints
+	if version := p.ramlDef["version"]; version != nil {
+		parseURL.Path = strings.Replace(parseURL.Path, "{version}", fmt.Sprintf("%v", version), 1)
+	}
+
+	ep.Host = parseURL.Hostname()
+	ep.BasePath = parseURL.Path
+	ep.Protocol = parseURL.Scheme
+
+	port, _ := strconv.Atoi(parseURL.Port())
+	if port == 0 {
+		port, _ = net.LookupPort("tcp", ep.Protocol)
+	}
+	ep.Port = int32(port)
+
+	if len(protocols) == 0 {
+		return append(endpoints, ep), err
+		// Overrides the protocol from the URI, but does not override the port.
+	} else if len(protocols) == 1 {
+		ep.Protocol = strings.ToLower(protocols[0])
+		if port == 0 {
+			port, _ = net.LookupPort("tcp", ep.Protocol)
+		}
+		ep.Port = int32(port)
+		return append(endpoints, ep), err
+	}
+	// With multiple protocols provided, ignores the port from the url.
+	for i := range protocols {
+		epCpy := endpointCopy(ep)
+		port, _ = net.LookupPort("tcp", protocols[i])
+		epCpy.Port = int32(port)
+		epCpy.Protocol = strings.ToLower(protocols[i])
+		endpoints = append(endpoints, epCpy)
+	}
+
+	return endpoints, err
+}
+
+func endpointCopy(e EndpointDefinition) EndpointDefinition {
+	ed := &e
+	return *ed
+}
+
+func validateRamlProtocols(protocols []interface{}) []string {
+	stringProtocols := []string{}
+	for i := range protocols {
+		p, ok := protocols[i].(string)
+		if !ok {
+			return []string{}
+		}
+		if strings.ToUpper(p) != "HTTPS" && strings.ToUpper(p) != "HTTP" {
+			return []string{}
+		}
+		stringProtocols = append(stringProtocols, p)
+	}
+	return stringProtocols
+}
