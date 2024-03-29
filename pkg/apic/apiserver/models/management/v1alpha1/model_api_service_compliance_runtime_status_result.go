8911e79f3663fa4d1d3b0dc7b7383245e1b47630
Vivek Singh Chauhan <vchauhan@axway.com>
2024-03-29 10:33:40 -0700

    APIGOV-27103 - update model to include risk count properties

--- pkg/apic/apiserver/models/management/v1alpha1/model_api_service_compliance_runtime_status_result.go
+++ pkg/apic/apiserver/models/management/v1alpha1/model_api_service_compliance_runtime_status_result.go
@@ -11,12 +11,12 @@ package management
 
 // ApiServiceComplianceRuntimeStatusResult APIService runtime results.
 type ApiServiceComplianceRuntimeStatusResult struct {
-	// The total number of critical errors in the runtime compliance result.
-	CriticalCount int32 `json:"criticalCount,omitempty"`
-	// The total number of errors in the runtime compliance result.
-	ErrorCount int32 `json:"errorCount,omitempty"`
-	// The total number of warning in the runtime compliance result.
-	WarningCount int32 `json:"warningCount,omitempty"`
+	// The count of high risk in the runtime compliance result.
+	HighCount int32 `json:"highCount,omitempty"`
+	// The count of medium risk in the runtime compliance result.
+	MediumCount int32 `json:"mediumCount,omitempty"`
+	// The count of low risk in the runtime compliance result.
+	LowCount int32 `json:"lowCount,omitempty"`
 	// Grade result from the results summary.
 	Grade string `json:"grade,omitempty"`
 }
