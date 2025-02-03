3d966beaee3a30a63d052989ac03407792303c97
Jason Collins <jcollins@axway.com>
2025-02-03 15:44:31 -0700

    skip registration if idp by name already registered

--- pkg/authz/oauth/providerregistry.go
+++ pkg/authz/oauth/providerregistry.go
@@ -51,6 +51,11 @@ func NewProviderRegistry() ProviderRegistry {
 
 // RegisterProvider - registers the provider using the config
 func (r *providerRegistry) RegisterProvider(idp corecfg.IDPConfig, tlsCfg corecfg.TLSConfig, proxyURL string, clientTimeout time.Duration) error {
+	if p, _ := r.providerMap.Get(idp.GetIDPName()); p != nil {
+		r.logger.WithField("name", idp.GetIDPName()).Debug("skipping registration of the same idp")
+		return nil
+	}
+
 	p, err := NewProvider(idp, tlsCfg, proxyURL, clientTimeout)
 	if err != nil {
 		return err
