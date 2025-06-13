6e0f24d46a56562dfc9bcfd39f15a43539ddbeae
Alin Rosca <acrosca@axway.com>
2025-06-13 14:38:02 +0300

    APIGOV-30406 check for endpoints

--- pkg/agent/handler/agentresource.go
+++ pkg/agent/handler/agentresource.go
@@ -107,7 +107,7 @@ func (h *agentResourceHandler) handleTraceabilitySampling(resource *v1.ResourceI
 		return err
 	}
 
-	if ta.Agentstate.Sampling == nil || !ta.Agentstate.Sampling.Enabled {
+	if ta.Agentstate.Sampling == nil || (!ta.Agentstate.Sampling.Enabled && len(ta.Agentstate.Sampling.Endpoints) == 0) {
 		return nil
 	}
 
