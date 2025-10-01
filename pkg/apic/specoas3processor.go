447252eaf2d6ff20ebd917384d99b6ac60c9ed70
Dragos Gabriel Ghinea <142506926+dgghinea@users.noreply.github.com>
2025-10-01 18:16:38 +0300

    APIGOV-31153 - add GetSpecBytes() + npe guards (#976)
    * APIGOV-31153 - add method + npe guards
    
    * APIGOV-31153 fix components npe
    
    ---------
    
    Co-authored-by: Alin Rosca <acrosca@axway.com>

--- pkg/apic/specoas3processor.go
+++ pkg/apic/specoas3processor.go
@@ -265,6 +265,12 @@ func (p *oas3SpecProcessor) AddSecuritySchemes(authSchemes map[string]interface{
 		if !ok {
 			continue
 		}
+		if p.spec.Components == nil {
+			p.spec.Components = &openapi3.Components{}
+		}
+		if p.spec.Components.SecuritySchemes == nil {
+			p.spec.Components.SecuritySchemes = openapi3.SecuritySchemes{}
+		}
 		p.spec.Components.SecuritySchemes[name] = &openapi3.SecuritySchemeRef{
 			Value: s,
 		}
