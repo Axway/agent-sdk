40df93ce24fff18300e0aa468e4899c52e776bf1
Alin Rosca <acrosca@axway.com>
2025-10-01 18:01:25 +0300

    APIGOV-31153 fix components npe

--- pkg/apic/specoas3processor.go
+++ pkg/apic/specoas3processor.go
@@ -265,6 +265,9 @@ func (p *oas3SpecProcessor) AddSecuritySchemes(authSchemes map[string]interface{
 		if !ok {
 			continue
 		}
+		if p.spec.Components == nil {
+			p.spec.Components = &openapi3.Components{}
+		}
 		if p.spec.Components.SecuritySchemes == nil {
 			p.spec.Components.SecuritySchemes = openapi3.SecuritySchemes{}
 		}
