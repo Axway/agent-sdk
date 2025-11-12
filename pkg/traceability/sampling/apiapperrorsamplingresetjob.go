381d77c1eb3776d323042cd082ad171d662da8ea
Alin Rosca <acrosca@axway.com>
2025-11-12 14:47:19 +0200

    APIGOV-31494 unique error sampling for api/app pair

---
+++ pkg/traceability/sampling/apiapperrorsamplingresetjob.go
@@ -0,0 +1,42 @@
+package sampling
+
+import (
+	"fmt"
+
+	"github.com/Axway/agent-sdk/pkg/jobs"
+	hc "github.com/Axway/agent-sdk/pkg/util/healthcheck"
+	"github.com/Axway/agent-sdk/pkg/util/log"
+)
+
+type apiAppErrorSamplingResetJob struct {
+	jobs.Job
+	logger log.FieldLogger
+}
+
+func newAPIAppErrorSamplingResetJob() *apiAppErrorSamplingResetJob {
+	logger := log.NewFieldLogger().
+		WithPackage("sdk.traceability").
+		WithComponent("apiAppErrorSamplingResetJob")
+	return &apiAppErrorSamplingResetJob{logger: logger}
+}
+
+func (j *apiAppErrorSamplingResetJob) Ready() bool {
+	status, _ := hc.GetGlobalStatus()
+	return status == string(hc.OK)
+}
+
+func (j *apiAppErrorSamplingResetJob) Status() error {
+	if status, _ := hc.GetGlobalStatus(); status != string(hc.OK) {
+		err := fmt.Errorf("agent is marked as not running")
+		j.logger.WithError(err).Trace("status failed")
+		return err
+	}
+	return nil
+}
+
+func (j *apiAppErrorSamplingResetJob) Execute() error {
+	agentSamples.samplingLock.Lock()
+	defer agentSamples.samplingLock.Unlock()
+	agentSamples.apiAppErrorSampling = make(map[string]bool)
+	return nil
+}
