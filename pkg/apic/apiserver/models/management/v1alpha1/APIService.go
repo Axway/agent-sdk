2d3a089fefab4bb60a813dc56409135aa041252d
Dragos Gabriel Ghinea <dgghinea@axway.com>
2025-12-15 14:37:29 +0200

    Revert "fix file"
    This reverts commit ad14d8f1e54f2dd7971d348b526c512995bbeeb5.

--- pkg/apic/apiserver/models/management/v1alpha1/APIService.go
+++ pkg/apic/apiserver/models/management/v1alpha1/APIService.go
@@ -52,7 +52,11 @@ func init() {
 type APIService struct {
 	apiv1.ResourceMeta
 	// GENERATE: The following code has been modified after code generation
-	// Agentdetails            ApiServiceAgentdetails `json:"agentdetails"`
+	//
+	// GENERATE: The following code has been modified after code generation
+	//// 	Agentdetails            ApiServiceAgentdetails `json:"agentdetails"`
+	Agentdetails *ApiServiceAgentdetails `json:"agentdetails,omitempty"`
+	Agentdetails *ApiServiceAgentdetails `json:"agentdetails,omitempty"`
 	Agentdetails *ApiServiceAgentdetails `json:"agentdetails,omitempty"`
 	// GENERATE: The following code has been modified after code generation
 	//
