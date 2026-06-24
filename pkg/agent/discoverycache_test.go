954995cbb6f0045fee0c5460ad8b3c603a36bf85
Jason Collins <jcollins@axway.com>
2026-06-24 09:28:16 -0700

    fix mocks

--- pkg/agent/discoverycache_test.go
+++ pkg/agent/discoverycache_test.go
@@ -194,6 +194,10 @@ type mockRIClient struct {
 	queryParams map[string]map[string]string
 }
 
+func (m *mockRIClient) GetAPIV1ResourceCount(_ string) (int, error) {
+	return 0, nil
+}
+
 func (m *mockRIClient) GetAPIV1ResourceInstances(query map[string]string, URL string) ([]*apiv1.ResourceInstance, error) {
 	if m.queryParams == nil {
 		m.queryParams = make(map[string]map[string]string)
