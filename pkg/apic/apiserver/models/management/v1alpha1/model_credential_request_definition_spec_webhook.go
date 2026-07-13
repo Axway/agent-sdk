3f4c538ddc91e8a10d0aa6616e5401017918e7c5
Vivek Singh Chauhan <vchauhan@axway.com>
2026-07-13 14:59:38 +0100

    APIGOV-33179 - v1alpha1 removed definitions

--- pkg/apic/apiserver/models/management/v1alpha1/model_credential_request_definition_spec_webhook.go
+++
@@ -1,9 +0,0 @@
-package management
-
-// GENERATE: All of the code below was replaced after code generation
-
-// CredentialRequestDefinitionSpecWebhook struct for CredentialRequestDefinitionSpecWebhook
-type CredentialRequestDefinitionSpecWebhook struct {
-	Kind string `json:"kind"`
-	Name string `json:"name"`
-}
